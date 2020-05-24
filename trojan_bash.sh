06a88b6a54e5d21c3f7ca1ca248f42c1e7f935f1
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-05-24 19:00:09 +0800

    Update trojan_bash.sh

--- trojan_bash.sh
+++ trojan_bash.sh
@@ -88,7 +88,6 @@ server {
     }
 }
 '                  >            /etc/nginx/conf.d/default.conf
-
 #修改系统控制文件启用BBR
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
