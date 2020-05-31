75f03ba619057f01677851d759a90637bbad9f39
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-05-31 13:57:25 +0800

    Update v2ray_bash.sh

--- v2ray_bash.sh
+++ v2ray_bash.sh
@@ -45,6 +45,8 @@ echo "0 0 1 */2 * service nginx stop; certbot renew; service nginx start;" | cro
 #关闭SELinux
 setsebool -P httpd_can_network_connect 1 && setenforce 0
 #下载探针
+#wget命令   -O不会创建目录    -P可以创建深层次目录
+mkdir    /home/website
 wget     https://raw.githubusercontent.com/kmvan/x-prober/master/dist/prober.php    -O     /home/website/p.php
 #创建nginx配置文件
 echo '
