8f9bb5cc02b44ecd03673f358e6ab6096e5b7419
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-01 01:09:35 +0800

    Create qBittorrent.sh

---
+++ qBittorrent.sh
@@ -0,0 +1,23 @@
+#安装qBittorrent  @  Debian 10
+apt   install  -y   qbittorrent-nox
+#为qbittorrent-nox创建一个systemd服务文件
+adduser --system --group  bt
+echo   ' 
+ [Unit]
+Description=qBittorrent Command Line Client
+After=network.target
+[Service]
+#Do not change to "simple"
+Type=forking
+User=bt
+Group=bt
+UMask=007
+ExecStart=/usr/bin/qbittorrent-nox -d
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target
+'        >          /etc/systemd/system/qbittorrent-nox.service
+systemctl daemon-reload
+systemctl enable qbittorrent-nox
+service  qbittorrent-nox    restart
+#前端登录地址domain:8080。用户名admin，密码adminadmin，默认下载目录/home/bt/Downloads/
