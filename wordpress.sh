15b21337390fcb65726626fcb733c86f193cceb0
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-01 15:34:26 +0800

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -39,8 +39,8 @@ listen 80;
 listen [::]:80;
 listen 443 ssl;
 listen [::]:443 ssl;
-ssl_certificate /etc/letsencrypt/live/www.example.com/fullchain.pem;  
-ssl_certificate_key /etc/letsencrypt/live/www.example.com/privkey.pem;   
+ssl_certificate       /etc/letsencrypt/live/fullchain.pem;  
+ssl_certificate_key   /etc/letsencrypt/live/privkey.pem;   
 if ( $scheme = http ){
 return 301 https://$server_name$request_uri;
 }
