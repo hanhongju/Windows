601b29dec689acb33dcebca6be81dd1aace934bc
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-03 20:42:48 +0800

    Update MinecraftJAVA.sh

--- MinecraftJAVA.sh
+++ MinecraftJAVA.sh
@@ -1,18 +1,17 @@
-#MineCraft JAVA版服务器搭建脚本@Debian 10
+#MineCraft JAVA版服务器搭建脚本@Debian 9
 apt update
 apt full-upgrade   -y
 apt autoremove     -y
 apt install        -y     wget   unzip   zip    screen   default-jdk
 #创建文件夹，搭建1.15.2版本服务器
 mkdir     /home/mcserver/
-cd        /home/mcserver/
 wget      https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar     -O   /home/mcserver/server.jar
 #启动服务器，更新最终许可文件
-java   -jar  /home/mcserver/server.jar    nogui
-sed   -i   's/eula=false/eula=true/g'   eula.txt
+java   -jar    /home/mcserver/server.jar    nogui
+sed    -i     's/eula=false/eula=true/g'   /home/mcserver/eula.txt
 #打开虚拟终端mc，后台托管mc
 screen   -S    mc
-java   -jar  /home/mcserver/server.jar    nogui
+java   -jar    /home/mcserver/server.jar    nogui
 #关闭shell，连接服务器，开玩
 
 
@@ -22,10 +21,11 @@ screen -X -S mc quit
 screen -ls
 
 
-#备份服务器，将mc目录压缩为mcserver.zip文件
-cd    /home/mcserver/
-zip   -r    ./mcserver.zip    ./*   -r
+#备份服务器，将world目录压缩为world.zip文件
+cd         /home/mcserver/world   
+zip   -r   /home/world.zip      ./*   
 #到新服务器，上传备份文件至/home/，创建mc文件夹,解压
-mkdir     /home/mcserver/
-unzip   -o   /home/mcserver.zip    -d     /home/mcserver/
+rm -rf     /home/mcserver/world/
+mkdir      /home/mcserver/world/
+unzip   -o   /home/world.zip     -d     /home/mcserver/world
 #打开服务器终端，开玩
