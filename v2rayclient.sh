c0f290f1025c2123b7d226e4430285e2894f9204
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-07 16:26:19 +0800

    Update v2rayclient.sh

--- v2rayclient.sh
+++ v2rayclient.sh
@@ -1,4 +1,4 @@
-#v2ray手动安装脚本@Debian 10
+#v2rayclient安装脚本@Debian 10
 #下载v2ray，如不能联网，手动下载v2ray至/home目录
 wget    -c     https://github.com/v2ray/v2ray-core/releases/download/v4.23.4/v2ray-linux-64.zip     -O     /home/v2ray-linux-64
 #解压移动v2ray文件至正确位置
