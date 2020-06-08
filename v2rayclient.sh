337bb607c0504c649734d7bad4937a9065636a6e
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-08 21:24:29 +0800

    Update v2rayclient.sh

--- v2rayclient.sh
+++ v2rayclient.sh
@@ -1,18 +1,20 @@
 #v2rayclient安装脚本@Debian 10
 
-#下载v2ray，如不能联网，手动下载v2ray至/home目录
+#下载v2ray，如不能联网，手动下载v2ray至/home目录或/home/hj目录
 wget    -c     https://github.com/v2ray/v2ray-core/releases/download/v4.23.4/v2ray-linux-64.zip     -O     /home/v2ray-linux-64.zip
-#解压移动v2ray文件至正确位置，上传配置文件config.json至/home目录
+#解压移动v2ray文件至正确位置，上传配置文件config.json至/home目录或/home/hj目录
 mkdir    /home/v2ray
-unzip   -o       /home/v2ray-linux-64.zip       -d          /home/v2ray
 mkdir    /usr/bin/v2ray/
 mkdir    /etc/v2ray/
+cp      -f       /home/hj/v2ray-linux-64.zip            /home
+unzip   -o       /home/v2ray-linux-64.zip       -d      /home/v2ray
 mv      -f       /home/v2ray/v2ray                      /usr/bin/v2ray/v2ray
 mv      -f       /home/v2ray/v2ctl                      /usr/bin/v2ray/v2ctl
 mv      -f       /home/v2ray/geoip.dat                  /usr/bin/v2ray/geoip.dat
 mv      -f       /home/v2ray/geosite.dat                /usr/bin/v2ray/geosite.dat
 mv      -f       /home/v2ray/config.json                /etc/v2ray/config.json
 mv      -f       /home/v2ray/systemd/v2ray.service      /etc/systemd/system/v2ray.service
+cp      -f       /home/hj/config.json                   /home/config.json
 cp      -f       /home/config.json                      /etc/v2ray/config.json
 rm      -rf      /home/v2ray
 systemctl daemon-reload
