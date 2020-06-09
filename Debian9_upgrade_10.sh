5c2d34b2b8874582896c84a39cf692b57840acf0
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-09 18:16:19 +0800

    Update Debian9_upgrade_10.sh

--- Debian9_upgrade_10.sh
+++ Debian9_upgrade_10.sh
@@ -1,9 +1,13 @@
 #Debian 9 升级为 Debian 10
-apt update -y && apt upgrade -y && apt dist-upgrade -y
+apt update
+apt full-upgrade  -y
+apt autoremove    -y
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 sed -i 's/stretch/buster/g' /etc/apt/sources.list 
 cat /etc/apt/sources.list
-apt update -y && apt upgrade -y && apt dist-upgrade -y
+apt update
+apt full-upgrade  -y
+apt autoremove    -y
 #查看当前系统版本
 lsb_release -a
 #查看当前nginx版本
