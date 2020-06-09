ad83d0386908f82fa49c9b9f67bcf7147d64fdae
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-09 18:19:53 +0800

    Update Debian9_upgrade_10.sh

--- Debian9_upgrade_10.sh
+++ Debian9_upgrade_10.sh
@@ -1,10 +1,8 @@
 #Debian 9 升级为 Debian 10
 apt update
 apt full-upgrade  -y
-apt autoremove    -y
 cp /etc/apt/sources.list /etc/apt/sources.list.bak
 sed -i 's/stretch/buster/g' /etc/apt/sources.list 
-cat /etc/apt/sources.list
 apt update
 apt full-upgrade  -y
 apt autoremove    -y
