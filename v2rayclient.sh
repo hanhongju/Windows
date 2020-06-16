4ba444c36addcabb737ba4b06e9b0357a23b351b
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-17 01:21:34 +0800

    Update v2rayclient.sh

--- v2rayclient.sh
+++ v2rayclient.sh
@@ -17,7 +17,7 @@ mv      -f       /home/v2ray/systemd/v2ray.service      /etc/systemd/system/v2ra
 systemctl   daemon-reload
 systemctl   enable      v2ray
 #导入节点信息文件
-cp      -f       /home/hj/config.json                      /etc/v2ray/config.json
+cp      -f       /home/hj/config.json                   /etc/v2ray/config.json
 cp      -f       /home/config.json                      /etc/v2ray/config.json
 #读取节点信息，重启
 systemctl   restart     v2ray
@@ -25,12 +25,8 @@ netstat -tulpna | grep 'v2ray'
 
 
 
-
-#设置终端代理
-export    ALL_PROXY=socks5://127.0.0.1:8000
-#测试外网连接
-curl    google.com
-
+#curl代理下载
+curl    -x   socks5://127.0.0.1:8000    -O    -C -    <URL>
 
 
 
