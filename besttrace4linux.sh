e31f1724988e936cc8bc5adba5ab04f5314aa692
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-16 23:59:11 +0800

    Create besttrace4linux.sh

---
+++ besttrace4linux.sh
@@ -0,0 +1,6 @@
+#BestTrace4Linux @Debian 10
+apt install -y unzip zip wget
+wget https://github.com/hanhongju/BestTrace-Linux/raw/master/besttrace4linux.zip
+unzip besttrace4linux.zip
+chmod +x besttrace
+./besttrace 1.63.2.132
