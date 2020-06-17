1567fd7021eb37aa2956d595ade1a08df9e2729c
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-18 01:52:49 +0800

    Update Syncthing.sh

--- Syncthing.sh
+++ Syncthing.sh
@@ -17,7 +17,7 @@ rm        -rf            /home/sync/
 #修改配置文件
 sed       -i        's/127.0.0.1/0.0.0.0/g'       /root/.config/syncthing/config.xml
 #后台运行sync
-screen   -R    sync
+screen    -R     sync
 /usr/bin/syncthing
 #关闭shell，连接IP:8384，webui配置同步文件夹
 
