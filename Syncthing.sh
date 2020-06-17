f81d818f3f495f4ec0db40f01b4cf5ec466cb2b9
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-18 01:14:33 +0800

    Update Syncthing.sh

--- Syncthing.sh
+++ Syncthing.sh
@@ -1,20 +1,33 @@
 #Syncthing安装脚本@Debian 10
 apt   update
-apt   install     -y    tar   screen
+apt   full-upgrade   -y
+apt   autoremove     -y
+apt   install        -y    tar   screen
 #wget   -c      https://github.com/syncthing/syncthing/releases/download/v1.6.1/syncthing-linux-amd64-v1.6.1.tar.gz       -O       /home/syncthing.tar.gz
 #安装文件
-mv        /home/syncthing-linux-amd64-v1.6.1.tar.gz               /home/syncthing.tar.gz 
 mkdir     /home/sync/
-cd           /home/sync/
-tar       -zxf       /home/syncthing.tar.gz           
+cd        /home/sync/
+tar       -zxf           /home/syncthing-linux-amd64-v1.6.1.tar.gz           
 cd        syncthing-linux-amd64-v1.6.1
-cp        syncthing             /usr/bin/syncthing
-rm      -rf          /home/sync/
-chmod               +x                    /usr/bin/syncthing
+cp        syncthing      /usr/bin/syncthing
+chmod     +x             /usr/bin/syncthing
+rm        -rf            /home/sync/
 #运行程序，生成配置文件
 /usr/bin/syncthing
 #修改配置文件
-sed   -i     's/127.0.0.1/0.0.0.0/g'     /root/.config/syncthing/config.xml
-#运行
+sed       -i        's/127.0.0.1/0.0.0.0/g'       /root/.config/syncthing/config.xml
+#后台运行sync
 screen   -R    sync
 /usr/bin/syncthing
+#关闭shell，连接IP:8384，webui配置同步文件夹
+
+
+
+#关闭所有screen终端和服务器
+screen -ls
+pkill screen
+screen -ls
+
+
+
+
