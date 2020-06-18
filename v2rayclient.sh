21f177ecf4b7530234889d9c618829cbc4869428
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-19 00:47:11 +0800

    Update v2rayclient.sh

--- v2rayclient.sh
+++ v2rayclient.sh
@@ -29,4 +29,14 @@ netstat -tulpna | grep 'v2ray'
 
 
 
+#设置tsocks透明代理
+apt  update
+apt  install    -y   tsocks
+echo '
+server = 127.0.0.1
+server_type = 5
+server_port = 8000
+'          >              /etc/tsocks.conf
+#在wget之前加上tsocks以使其通过代理
+
 
