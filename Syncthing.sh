b3a54667511d92838a44dc235a3eaf2367fe8092
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-20 14:33:44 +0800

    Update Syncthing.sh

--- Syncthing.sh
+++ Syncthing.sh
@@ -2,36 +2,27 @@
 apt   update
 apt   full-upgrade   -y
 apt   autoremove     -y
-apt   install        -y    tar wget unzip zip screen
+apt   install        -y    tar wget unzip zip
 wget   -c      https://github.com/syncthing/syncthing/releases/download/v1.6.1/syncthing-linux-amd64-v1.6.1.tar.gz       -P       /home/
 #安装文件
 mkdir     /home/sync/
 cd        /home/sync/
 tar       -zxf           /home/syncthing-linux-amd64-v1.6.1.tar.gz           
 cd        syncthing-linux-amd64-v1.6.1
-cp        syncthing      /usr/bin/syncthing
+cp        syncthing                                           /usr/bin/syncthing
+cp        etc/linux-systemd/system/syncthing@.service         /etc/systemd/system/syncthing@.service   
 chmod     +x             /usr/bin/syncthing
 rm        -rf            /home/sync/
 #运行程序，生成配置文件
-screen    -R     sync
-/usr/bin/syncthing
-#关闭shell，重新连接
+systemctl daemon-reload
+systemctl enable syncthing@root.service
+service         syncthing@root           start
+service         syncthing@root           stop
 #修改配置文件
 sed       -i        's/127.0.0.1/0.0.0.0/g'       /root/.config/syncthing/config.xml
-#后台运行sync
-pkill      syncthing
-screen    -R     sync
-/usr/bin/syncthing
-#关闭shell，连接IP:8384，webui配置同步文件夹
-
-
-
-#关闭所有screen终端和服务器
+service         syncthing@root           restart
 netstat -tulpna | grep 'syncthing'
-screen -ls
-pkill screen
-
-
+#回显syncthing监听端口
 
 
 
