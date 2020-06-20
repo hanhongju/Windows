b1fb8608f1d0bcf79be61ad65ffc5e9ac528a200
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-20 15:30:09 +0800

    Update shadowsocks.sh

--- shadowsocks.sh
+++ shadowsocks.sh
@@ -24,17 +24,25 @@ netstat -tulpna | grep 'ss-server'
 #shadowsocks客户端使用脚本@Debian 10
 #安装shadowsocks
 apt      update
-apt      install     -y    shadowsocks-libev screen net-tools
-#screen中启动ss客户端
-screen     -R   ss
-ss-local   -p   3389   -l  8000   -k   fengkuang  -t   60   -m   aes-256-gcm   -s  <serverdomain>
-#关闭shell，启动软件，设置代理
+apt      install     -y    shadowsocks-libev  net-tools
+#写入服务器信息
+echo   '
+{
+  "server": "<serverdomain>",
+  "server_port": "3389",
+  "local_port": 8000,
+  "password": "fengkuang",
+  "timeout": 60,
+  "method": "aes-256-gcm"
+}
+'         >        /etc/shadowsocks-libev/root.json
+#启动服务
+systemctl   enable      shadowsocks-libev-local@root
+systemctl   restart     shadowsocks-libev-local@root
+netstat    -tulpna | grep 'ss-local'
+#回显ss监听端口
 
 
-#关闭shadowsocks客户端
-netstat -tulpna | grep 'ss-local'
-screen         -ls
-pkill    screen    
 
 
 
