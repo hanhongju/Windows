2d2c21e16a748903f15602d5b947a93eade0d6ac
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-21 21:52:50 +0800

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -61,15 +61,15 @@ service   nginx   restart
 #配置数据库
 mysql_secure_installation
 #修改数据库登录方式
-mysql      -uroot    -pfengkuang     -e      "update mysql.user set plugin='mysql_native_password' where User='root'"
+mysql      -uroot     -pfengkuang     -e      "update mysql.user set plugin='mysql_native_password' where User='root'"
 #创建新数据库
-mysql      -uroot    -pfengkuang     -e      "DROP DATABASE wordpress"
-mysql      -uroot    -pfengkuang     -e      "CREATE DATABASE wordpress"
+mysql      -uroot     -pfengkuang     -e      "DROP DATABASE wordpress"
+mysql      -uroot     -pfengkuang     -e      "CREATE DATABASE wordpress"
 #重启服务
-systemctl   enable   nginx 
-systemctl   enable   mariadb
-service     nginx    restart
-service     mariadb  restart
+systemctl     enable       nginx 
+systemctl     enable       mariadb
+systemctl     restart      nginx
+systemctl     restart      mariadb
 #浏览器进入网站开始配置
 #设置-固定链接，改为朴素
 
@@ -77,9 +77,9 @@ service     mariadb  restart
 
 
 #转移网站->备份数据库，存放于db_dump.sql
-mysqldump  -uroot    -pfengkuang     wordpress   >    /home/db_dump.sql
+mysqldump  -uroot     -pfengkuang     wordpress   >    /home/db_dump.sql
 #将备份文件导入数据库
-mysql      -uroot    -pfengkuang     wordpress   <    /home/db_dump.sql
+mysql      -uroot     -pfengkuang     wordpress   <    /home/db_dump.sql
 
 
 
@@ -91,7 +91,7 @@ sed      -i       ''s/127.0.0.1/\*/g''         /etc/mysql/mariadb.conf.d/50-serv
 service     mariadb  restart
 netstat    -an | grep 3306
 #添加外网连接数据库权限
-mysql      -uroot    -pfengkuang     -e      "use mysql; grant all privileges on *.* to 'root'@'%' identified by 'fengkuang' with grant option; flush privileges; select user,host from user;"
+mysql      -uroot     -pfengkuang     -e      "use mysql; grant all privileges on *.* to 'root'@'%' identified by 'fengkuang' with grant option; flush privileges; select user,host from user;"
 #至此mysql服务开放给公网
 
 
