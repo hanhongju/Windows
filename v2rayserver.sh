4842732586a207a6736f680eb972499d1fa42c5f
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-22 00:55:49 +0800

    Update v2rayserver.sh

--- v2rayserver.sh
+++ v2rayserver.sh
@@ -12,7 +12,7 @@ pip3   install     certbot
 bash    -c     "$(curl -L -s https://install.direct/go.sh)"
 #申请SSL证书
 service     nginx       stop
-certbot     certonly    --standalone    --agree-tos   -n     -d    $site     -m    86606682@qq.com 
+certbot     certonly    --standalone    --agree-tos     -n     -d      $site     -m    86606682@qq.com 
 #配置证书自动更新
 echo      "0 0 1 */2 * service nginx stop; certbot renew; service nginx start;"   |   crontab
 crontab   -l
