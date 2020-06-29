9d1ca536bba27490708a3a79c71abe11ff4d500b
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-29 23:11:34 +0800

    Update besttrace4linux.sh

--- besttrace4linux.sh
+++ besttrace4linux.sh
@@ -1,9 +1,9 @@
 #BestTrace4Linux 查看VPS回程节点@Debian 10
-apt     install    -y     unzip zip wget
-mkdir   /home/besttrace
-cd      /home/besttrace
-wget    -c   https://raw.githubusercontent.com/hanhongju/my_script/master/besttrace4linux.zip    -O    /home/besttrace4linux.zip
-unzip   -o   /home/besttrace4linux.zip    -d    /home/besttrace
-chmod   +x   besttrace
-./besttrace      1.63.2.132
+apt        install    -y     unzip zip wget
+mkdir      /home/besttrace
+cd         /home/besttrace
+wget       https://raw.githubusercontent.com/hanhongju/my_script/master/besttrace4linux.zip    -O    /home/besttrace4linux.zip
+unzip      -o     /home/besttrace4linux.zip    -d    /home/besttrace
+chmod      +x     besttrace
+./besttrace       1.63.2.132
 
