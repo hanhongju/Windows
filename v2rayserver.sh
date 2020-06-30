c17c65ffd9061b97580dbd7643b4e4287998a46c
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-06-30 16:01:49 +0800

    Update v2rayserver.sh

--- v2rayserver.sh
+++ v2rayserver.sh
@@ -1,6 +1,7 @@
 #v2rayserver安装脚本@Debian 10
 #定义网站地址
-echo    "本脚本可以自动安装v2ray，自动申请并使用tls证书加密保护v2ray的流量，反代美国国家生物技术信息中心网址进行网站伪装。需要您事先将此VPS的IP地址解析到一个有效域名上。
+echo    "
+本脚本可以自动安装v2ray，自动申请并使用tls证书加密保护v2ray的流量，反代美国国家生物技术信息中心网址进行网站伪装。需要您事先将此VPS的IP地址解析到一个有效域名上。
 如果您有多个域名解析到此VPS，请重复运行此脚本并输入不同的域名，那么多个域名地址都可以受到tls的加密保护。如果此VPS使用KVM虚拟技术，此脚本自动开启BBR加速。
 安装完成后
 端口为             443
@@ -9,11 +10,27 @@ echo    "本脚本可以自动安装v2ray，自动申请并使用tls证书加密
 传输协议为         ws
 路径为             /f63lKAx
 底层传输安全为     tls
-理解并记录下这些信息后请按回车键继续，并在下一栏输入您解析的有效域名。如果域名输入有误请按Ctrl+C终止脚本运行，然后重新运行脚本。"
+理解并记录下这些信息后请按回车键继续，并在下一栏输入您解析的有效域名。如果域名输入有误请按Ctrl+C终止脚本运行，然后重新运行脚本。
+This script can automatically setup v2ray, register a tls certification and protect your v2ray stream. Nginx installed can reverse proxy united states national biotechnology information cent to mask your website. This script requires you have a domain which had been resolved to IP of this VPS.
+If you have several domains resolved to this VPS, you could repeat running this script and type in different domains which could all be protected by tls. If your VPS is based on a KVM virtualization technology, this script can automatically enable BBR acceleration.
+After your running this script, your:
+port is                              443
+user ID is                           15448fce-7c71-11ea-bc55-0242ac130003
+transport protocol is                ws
+path is                              /f63lKAx
+milestone transport security is      tls.
+Understand and record this information, then press enter to proceed. Type in your available domain. If you type in a wrong code, you can press Ctrl + C to cancer transcript running, after which, you can run script again to restart setup.
+"
 read    nothing
-echo    "请输入此VPS的IP对应的域名地址："
+echo    "
+请输入此VPS的IP对应的域名地址：
+Now type in your VPS's domain:
+"
 read    site
-echo    "好的，现在要开始安装了。"
+echo    "
+好的，现在要开始安装了。
+OK, it is about to start.
+"
 sleep   5s
 
 
@@ -133,8 +150,14 @@ netstat  -plunt | grep 'nginx'
 OUTPUT=$(nginx -t 2>&1)
 echo   $OUTPUT
 if     [[  "$OUTPUT"   =~   "successful"   ]]   ;        
-then        echo   "至此，v2ray可正常工作。"
-else        echo   "您输入的域名地址可能没有正确解析或者短时间申请了太多的证书，不能正常申请证书，所以nginx不能正常工作。现在所有nginx配置都已被删除。在您确认了域名解析没有问题后再请重新运行本脚本。"
+then        echo   "
+至此，v2ray可正常工作。
+Now, your v2ray can work properly.
+"
+else        echo   "
+您输入的域名地址可能没有正确解析或者短时间申请了太多的证书，不能正常申请证书，所以nginx不能正常工作。现在所有nginx配置都已被删除。在您确认了域名解析没有问题后再请重新运行本脚本。
+The domain u gave cannot be properly resolved or had registered too many certifications, so that script cannot registered properly and nginx cannot work properly. Now all nginx configurations had been deleted. Please re-run this script after confirmation of availability of your domain.
+"
             rm    -rf    /etc/nginx/sites-enabled/*
 fi
 finish_time=$(date +%s)
