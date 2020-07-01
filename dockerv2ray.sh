1be41e13546bda4250341df61f736d387f4886b6
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-01 21:38:24 +0800

    Update dockerv2ray.sh

--- dockerv2ray.sh
+++ dockerv2ray.sh
@@ -8,9 +8,7 @@ read    site
 #拉取v2ray脚本并安装
 apt      purge      -y      apache2
 docker   rm         -f      v2ray
-docker   run        -d      --rm --name v2ray -p 443:443 -p 80:80 -v $HOME/.caddy:/root/.caddy  pengchujin/v2ray_ws:0.10   $site V2RAY_WS 15448fce-7c71-11ea-bc55-0242ac130003
-#显示信息
-docker   logs    v2ray
+docker   run        -d      --rm --name v2ray -p 443:443 -p 80:80 -v $HOME/.caddy:/root/.caddy  pengchujin/v2ray_ws:0.10   $site V2RAY_WS 15448fce-7c71-11ea-bc55-0242ac130003  &&  docker   logs    v2ray
 
 
 
