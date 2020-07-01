0db87747739f1a9f6a0dded74ef6b58ee56f05e7
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-02 02:09:25 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -29,7 +29,7 @@ apt update && apt install -y curl && bash -c "$(curl -sL https://get.docker.com)
 #定义站点地址
 site=hostc.hongju.site
 #拉取v2ray脚本并安装
-apt       purge       -y       apache2
+apt    purge    -y       apache2
 docker    rm    -f     v2ray
 docker    run   -d   --rm --name v2ray -p 443:443 -p 80:80 -v $HOME/.caddy:/root/.caddy  pengchujin/v2ray_ws:0.10   $site V2RAY_WS 15448fce-7c71-11ea-bc55-0242ac130003  
 sleep     3s
