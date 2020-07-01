78d0c8a81e858484637cd42f3200d854d060c817
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-01 21:03:47 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -13,3 +13,14 @@ docker   run   hello-world
 docker     --version
 docker-compose    --version
 
+
+
+
+
+site=
+#安装docker
+apt update && apt install -y curl && bash -c "$(curl -sL https://get.docker.com)"
+#拉取v2ray脚本并安装
+docker run -d --rm --name v2ray -p 443:443 -p 80:80 -v $HOME/.caddy:/root/.caddy  pengchujin/v2ray_ws:0.10 $site V2RAY_WS 15448fce-7c71-11ea-bc55-0242ac130003
+#显示信息
+docker logs v2ray
