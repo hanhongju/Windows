12ebeaeb4815ccb5af48963f361ca3693dc51586
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-01 13:36:18 +0800

    Update v2rayclient.sh

--- v2rayclient.sh
+++ v2rayclient.sh
@@ -5,22 +5,17 @@ wget        https://github.com/v2ray/v2ray-core/releases/download/v4.23.4/v2ray-
 #安装v2ray文件
 apt   update
 apt   install    -y      unzip  zip  curl   wget
-mkdir    /home/v2ray
 mkdir    /usr/bin/v2ray/
 mkdir    /etc/v2ray/
-cp      -f       /home/hj/v2ray-linux-64.zip            /home/v2ray-linux-64.zip
-unzip   -o       /home/v2ray-linux-64.zip       -d      /home/v2ray
-mv      -f       /home/v2ray/v2ray                      /usr/bin/v2ray/v2ray
-mv      -f       /home/v2ray/v2ctl                      /usr/bin/v2ray/v2ctl
-mv      -f       /home/v2ray/geoip.dat                  /usr/bin/v2ray/geoip.dat
-mv      -f       /home/v2ray/geosite.dat                /usr/bin/v2ray/geosite.dat
+cp        -f       /home/hj/v2ray-linux-64.zip                  /home/v2ray-linux-64.zip
+unzip     -o       /home/v2ray-linux-64.zip          -d         /usr/bin/v2ray/
 #配置v2ray服务
-mv      -f       /home/v2ray/systemd/v2ray.service      /etc/systemd/system/v2ray.service
-systemctl   daemon-reload
-systemctl   enable      v2ray
+cp        -f       /usr/bin/v2ray/systemd/v2ray.service         /etc/systemd/system/v2ray.service
+systemctl     daemon-reload
+systemctl     enable      v2ray
 #导入节点信息文件
-cp      -f       /home/hj/config.json                   /home/config.json
-cp      -f       /home/config.json                      /etc/v2ray/config.json
+cp        -f       /home/hj/config.json                         /home/config.json
+cp        -f       /home/config.json                            /etc/v2ray/config.json
 #读取节点信息，重启
 service    v2ray     restart     
 sleep 1s
