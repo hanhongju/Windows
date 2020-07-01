4009b60f8ff28896ed9917eaf47e406468a0f405
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-01 14:56:43 +0800

    Update besttrace4linux.sh

--- besttrace4linux.sh
+++ besttrace4linux.sh
@@ -6,7 +6,7 @@ cd         /home/besttrace
 wget       https://raw.githubusercontent.com/hanhongju/my_script/master/besttrace4linux.zip    -O    /home/besttrace4linux.zip
 unzip      -o     /home/besttrace4linux.zip    -d    /home/besttrace
 chmod      +x     besttrace
-./besttrace       1.63.2.132
+./besttrace       60.218.218.145
 
 
 
