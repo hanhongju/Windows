cc2580389e1a444747e327fb8dab22d63181d95d
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-02 15:09:26 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -9,8 +9,8 @@ apt update && apt install -y curl && bash -c "$(curl -sL https://get.docker.com)
 cp        -f       /home/hj/config.json                         /home/config.json
 cp        -f       /home/config.json                            /etc/v2rayconfig.json
 #读取节点信息，启动容器
-docker    rm    -f      v2ray
-docker    run   -d    --name    v2ray    -v    /etc:/etc/v2ray    -p   8000:8000   v2ray/official   v2ray   -config=/etc/v2ray/v2rayconfig.json
+docker    rm    -f     v2ray
+docker    run   -d    --name    v2ray    -p   8000:8000   -v   /etc:/etc/v2ray    v2ray/official   v2ray   -config=/etc/v2ray/v2rayconfig.json
 docker    container    ls
 #回显容器信息
 
@@ -25,7 +25,7 @@ site=<domain>
 #拉取v2ray脚本并安装
 apt    purge    -y     apache2
 docker    rm    -f     v2ray
-docker    run   -d   --rm --name v2ray -p 443:443 -p 80:80 -v $HOME/.caddy:/root/.caddy  pengchujin/v2ray_ws:0.10   $site V2RAY_WS 15448fce-7c71-11ea-bc55-0242ac130003  
+docker    run   -d    --name    v2ray    -p   443:443   -p   80:80   -v   $HOME/.caddy:/root/.caddy    pengchujin/v2ray_ws:0.10   $site   V2RAY_WS  15448fce-7c71-11ea-bc55-0242ac130003  
 sleep     3s
 docker    logs         v2ray
 docker    container    ls
