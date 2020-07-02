2edf46a63aee84871d470cadb0db028023d7483c
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-02 15:02:08 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -20,18 +20,18 @@ docker    container    ls
 
 
 #Docker安装v2ray+tls server@Debian 10
-apt    update
-apt    install    -y       curl
-apt    purge      -y       apache2
 #定义站点地址
 site=<domain>
 #拉取v2ray脚本并安装
+apt    purge    -y     apache2
 docker    rm    -f     v2ray
 docker    run   -d   --rm --name v2ray -p 443:443 -p 80:80 -v $HOME/.caddy:/root/.caddy  pengchujin/v2ray_ws:0.10   $site V2RAY_WS 15448fce-7c71-11ea-bc55-0242ac130003  
 sleep     3s
 docker    logs         v2ray
 docker    container    ls
 #显示服务器配置
+
+
 
 
 
