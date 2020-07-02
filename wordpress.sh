a4c7bcc2988e8c5ab121ac343879f95a562d442e
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-02 21:18:19 +0800

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -20,11 +20,9 @@ setsebool -P httpd_can_network_connect 1 && setenforce 0
 wget   -c   https://raw.githubusercontent.com/kmvan/x-prober/master/dist/prober.php     -O     /home/website/p.php
 #下载wordpress至网站根目录
 wget       -c         https://cn.wordpress.org/latest-zh_CN.tar.gz      -P     /home/website/
-chmod       777      -R       /home/website/
 cd         /home/website   
 tar         zxf       latest-zh_CN.tar.gz
-mv          wordpress/*  .  
-rm         -rf        wordpress     latest-zh_CN.tar.gz
+chmod       777      -R       /home/website/
 #创建nginx配置文件
 echo '
 server {
@@ -38,8 +36,8 @@ ssl_certificate_key   /etc/letsencrypt/live/www.example.com/privkey.pem;
 if ( $scheme = http ){
 return 301 https://$server_name$request_uri;
 }
-root   /home/website/;
-index   index.php index.html index.htm;
+root      /home/website/wordpress/;
+index      index.php index.html index.htm;
 location ~ \.php$ {
 fastcgi_pass  unix:/run/php/php7.3-fpm.sock;     #php -v 遇到502 Bad Gateway时查看php版本，确认php-fpm.sock版本
 fastcgi_index  index.php;
@@ -77,10 +75,11 @@ mysql         -uroot     -pfengkuang     -e      "update mysql.user set plugin='
 mysql         -uroot     -pfengkuang     -e      "DROP DATABASE wordpress"
 mysql         -uroot     -pfengkuang     -e      "CREATE DATABASE wordpress"
 mysql         -uroot     -pfengkuang     -e      "SHOW DATABASEs"
-#开放数据库3306端口给外网
+#开放数据库给外网
+<<BLOCK
 sed      -i       ''s/127.0.0.1/\*/g''         /etc/mysql/mariadb.conf.d/50-server.cnf
-#开放数据库用户权限给外网
 mysql         -uroot     -pfengkuang     -e      "use mysql; grant all privileges on *.* to 'root'@'%' identified by 'fengkuang' with grant option; flush privileges; select user,host from user;"
+BLOCK
 systemctl     enable       mariadb
 systemctl     restart      mariadb
 netstat      -plunt    |   grep   3306
