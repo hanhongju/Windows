a0f93a0c7b7ead616459ce09a273ad56046c13e2
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-05 18:33:55 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -1,6 +1,6 @@
 #trojan安装脚本@Debian 10
 #定义网站地址
-echo    "本脚本可以自动安装trojan，自动申请并使用tls证书加密保护trojan的流量，反代美国国家生物技术信息中心网址进行网站伪装。需要您事先将此VPS的IP地址解析到一个有效域名上。
+echo    "本脚本可以自动安装trojan，自动申请并使用tls证书加密保护trojan的流量，反代朝鲜劳动新闻网址进行网站伪装。需要您事先将此VPS的IP地址解析到一个有效域名上。
 如果此VPS使用KVM虚拟技术，此脚本自动开启BBR加速。
 理解这些信息后请按回车键继续，并在下一栏输入您解析的有效域名。如果域名输入有误请按Ctrl+C终止脚本运行，然后重新运行脚本。"
 read    nothing
@@ -22,7 +22,7 @@ apt    update
 apt    full-upgrade    -y
 apt    autoremove      -y
 apt    purge           -y         apache2
-apt    install         -y         python3-pip wget curl net-tools policycoreutils nginx ntp ntpdate
+apt    install         -y         python3-pip wget curl net-tools policycoreutils ntp ntpdate
 #安装Certbot
 pip3   install     cryptography --upgrade
 pip3   install     certbot
@@ -37,41 +37,6 @@ net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
 '         >       /etc/sysctl.conf
 sysctl   -p
-#修改nginx配置文件
-echo '
-events {
-worker_connections  1024;
-}
-http {
-sendfile    on;
-keepalive_timeout  65;
-server {
-listen 127.0.0.1:80;
-location / {
-proxy_pass https://pubmed.ncbi.nlm.nih.gov;
-}
-}
-server {
-listen 0.0.0.0:80;
-listen [::]:80;
-location / {
-return 301 https://$host$request_uri;
-}
-}
-}
-'         >       /etc/nginx/nginx.conf
-
-
-
-
-
-
-
-
-
-
-
-
 #申请SSL证书
 service     nginx       stop
 certbot     certonly    --standalone    --agree-tos     -n     -d      $site     -m    86606682@qq.com 
@@ -86,7 +51,7 @@ echo '
     "run_type": "server",
     "local_addr": "::",
     "local_port": 443,
-    "remote_addr": "127.0.0.1",
+    "remote_addr": "www.rodong.rep.kp",
     "remote_port": 80,
     "password": ["defaultpassword"],
     "ssl": {
@@ -99,13 +64,10 @@ sed    -i     ''s/www.example.com/$site/g''              /usr/local/etc/trojan/c
 sed    -i     ''s/defaultpassword/$password/g''          /usr/local/etc/trojan/config.json
 #启动trojan和Nginx
 systemctl    enable    trojan
-systemctl    enable    nginx
 service      trojan    restart
-service      nginx     restart
 #显示监听端口
-netstat -tulpna | grep 'nginx\|trojan'
-OUTPUT=$(netstat -tulpna | grep 'nginx\|trojan'    2>&1)
-nginx -t
+netstat -tulpna | grep 'trojan'
+OUTPUT=$(netstat -tulpna | grep 'trojan'    2>&1)
 if     [[  "$OUTPUT"   =~   "trojan"   ]]   ;        
 then        echo   "至此，trojan可正常工作。"
 else        echo   "您输入的域名地址可能没有正确解析或者短时间申请了太多的证书，不能正常申请证书，所以trojan不能正常工作。在您确认了域名解析没有问题后再请重新运行本脚本。"
