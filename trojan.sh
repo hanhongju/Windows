8127b1008c3177eac2e9c9c9d519426f67508e90
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-10 14:36:13 +0800

    Update trojan.sh

--- trojan.sh
+++ trojan.sh
@@ -36,6 +36,10 @@ crontab    -l
 service     nginx         stop
 service     apache2       stop
 certbot     certonly    --standalone    --agree-tos     -n     -d      $site     -m    86606682@qq.com 
+rm  -rf     /home/keys/
+mkdir       /home/keys/
+ln    -s    /etc/letsencrypt/live/$site/fullchain.pem       /home/keys/fullchain.pem
+ln    -s    /etc/letsencrypt/live/$site/privkey.pem         /home/keys/privkey.pem
 #安装trojan
 rm           -rf        /usr/local/etc/trojan/config.json               /etc/systemd/system/trojan.service
 bash         -c         "$(curl -fsSL https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh)"
@@ -51,12 +55,11 @@ echo '
     "remote_port": 80,
     "password": ["fengkuang"],
     "ssl": {
-        "cert": "/etc/letsencrypt/live/www.example.com/fullchain.pem",
-        "key": "/etc/letsencrypt/live/www.example.com/privkey.pem"
+        "cert": "/home/keys/fullchain.pem",
+        "key": "/home/keys/privkey.pem"
     }
 }
 '           >          /usr/local/etc/trojan/config.json
-sed    -i     ''s/www.example.com/$site/g''              /usr/local/etc/trojan/config.json
 #启动trojan
 systemctl    enable     trojan
 systemctl    restart    trojan
