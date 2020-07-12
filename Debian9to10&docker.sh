5cf3c9926e08a231bc6ffbca5256e97569cb37b5
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-12 23:31:30 +0800

    Update Debian9to10&docker.sh

--- Debian9to10&docker.sh
+++ Debian9to10&docker.sh
@@ -13,7 +13,7 @@ apt   autoremove      -y
 apt  update
 apt  install  -y      apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl  -fsSL     https://download.docker.com/linux/ubuntu/gpg |  apt-key add -
-add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/debian    $(lsb_release -cs)    stable"
+add-apt-repository    "deb [arch=amd64] https://mirrors.ustc.edu.cn/docker-ce/linux/debian    $(lsb_release -cs)    stable"
 apt  update
 apt  install  -y      docker-ce docker-ce-cli containerd.io
 docker run hello-world
