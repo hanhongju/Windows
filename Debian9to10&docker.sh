139dcb5b3b8f7ee3aa65b82e44088620feedeb9e
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-12 19:54:10 +0800

    Update Debian9to10&docker.sh

--- Debian9to10&docker.sh
+++ Debian9to10&docker.sh
@@ -4,7 +4,7 @@ apt   full-upgrade    -y
 cp    /etc/apt/sources.list           /etc/apt/sources.list.bak
 sed   -i    's/stretch/buster/g'      /etc/apt/sources.list 
 apt   update
-apt   full-upgrade    -y
+apt   full-upgrade    -y    --fix-missing
 apt   autoremove      -y
 
 
