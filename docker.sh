812d7d98cf01d726675631d6d34d43bd39ac766f
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-12 14:54:17 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -1,9 +1,10 @@
 #Docker安装脚本@Debian 10
-apt  install    -y      apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+apt  update
+apt  install  -y      apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl  -fsSL     https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository    "deb [arch=amd64] https://download.docker.com/linux/debian    $(lsb_release -cs)    stable"
 apt  update
-apt  install    -y      docker-ce docker-ce-cli containerd.io
+apt  install  -y      docker-ce docker-ce-cli containerd.io
 docker run hello-world
 #Docker said hello
 
