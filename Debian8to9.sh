79f0b9b3a0feca16bd406ba9340fdf7a97cb6b6d
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-13 13:24:53 +0800

    Update Debian8to9.sh

--- Debian8to9.sh
+++ Debian8to9.sh
@@ -2,7 +2,7 @@
 apt   update
 apt   full-upgrade    -y
 cp    /etc/apt/sources.list           /etc/apt/sources.list.bak
-sed -i 's/jessie/stretch/g' /etc/apt/sources.list
+sed   -i    's/jessie/stretch/g'      /etc/apt/sources.list
 apt   update
 apt   full-upgrade    -y    --fix-missing
 apt   autoremove      -y
