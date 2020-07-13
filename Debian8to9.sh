75ea228085300e657ec8746a0b00d4d91dfd8d34
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-13 13:24:12 +0800

    Create Debian8to9.sh

---
+++ Debian8to9.sh
@@ -0,0 +1,15 @@
+#Debian 8 升级为 Debian 9
+apt   update
+apt   full-upgrade    -y
+cp    /etc/apt/sources.list           /etc/apt/sources.list.bak
+sed -i 's/jessie/stretch/g' /etc/apt/sources.list
+apt   update
+apt   full-upgrade    -y    --fix-missing
+apt   autoremove      -y
+lsb_release   -a
+#显示系统新版本信息
+
+
+
+
+
