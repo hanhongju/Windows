fd799e4e2327a30c7e69bbbe7f3f86a7ea50b2a4
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-19 02:00:20 +0800

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -6,9 +6,9 @@ apt   autoremove     -y
 apt   install        -y      wget curl zip unzip net-tools nginx php-fpm php-mysql mariadb-server  
 #apt  install        -y      php-zip php-dom php-mbstring php-gd php-curl
 #安装wordpress网页文件
+wget        https://cn.wordpress.org/latest-zh_CN.tar.gz     -cP      /home/
 rm         -rf       /home/wordpress/
 cd         /home/
-wget       -c         https://cn.wordpress.org/latest-zh_CN.tar.gz
 tar         zxf       latest-zh_CN.tar.gz
 chmod      -Rf        777           /home/
 chown      -Rf        www-data      /home/
