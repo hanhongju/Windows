fa73856a02b889b6f4e743a08a89cdc4e1ac9821
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-19 11:21:58 +0800

    Update minecraft_bedrock.sh

--- minecraft_bedrock.sh
+++ minecraft_bedrock.sh
@@ -24,12 +24,11 @@ pkill -9 bedrock_server
 
 
 #备份服务器，将worlds目录压缩为worlds.zip文件
-cd         /home/mcserver/worlds
-zip     -q     /home/worlds.zip     -r     ./*   
+cd      /home/mcserver/
+zip     -q      /home/worlds.zip     -r     ./worlds/
 #到新服务器，上传备份文件至/home/，创建mc文件夹,解压
-rm    -rf     /home/mcserver/worlds
-mkdir         /home/mcserver/worlds
-unzip   -qo   /home/worlds.zip     -d     /home/mcserver/worlds
+rm      -rf     /home/mcserver/worlds
+unzip   -qo     /home/worlds.zip     -d     /home/mcserver/
 #启动服务器，开玩
 #win10版MC本地数据库文件在：
 #C:\Users\hongju\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds
