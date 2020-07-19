ae5d473c66377ebd1b8531e8a68e1acac9d2e920
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-19 11:08:57 +0800

    Update minecraft_JAVA.sh

--- minecraft_JAVA.sh
+++ minecraft_JAVA.sh
@@ -8,13 +8,11 @@ echo   'server {listen 80;listen [::]:80;root /home/;}'   >   /etc/nginx/sites-e
 service   nginx   restart
 #下载1.15.2版本mc服务器
 wget      https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar     -cP     /home/mcserver/
-#启动服务器，更新最终许可文件
-cd       /home/mcserver/
-java     -jar    server.jar    nogui
-sed      -i     's/eula=false/eula=true/g'       eula.txt
 #打开虚拟终端mc，后台托管mc
 screen   -R      mc
 cd       /home/mcserver/
+java     -jar    server.jar    nogui
+sed      -i     's/eula=false/eula=true/g'       eula.txt
 java     -jar    server.jar    nogui
 #关闭shell，连接服务器，开玩
 
