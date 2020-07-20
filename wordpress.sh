5ea186bf776610f834ed6cc0ec74c8fe35832d56
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-21 01:35:40 +0800

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -66,10 +66,10 @@ mysql         -uroot     -pfengkuang     -e      "SHOW DATABASEs"
 sed      -i       ''s/127.0.0.1/\*/g''         /etc/mysql/mariadb.conf.d/50-server.cnf
 mysql         -uroot     -pfengkuang     -e      "use mysql; grant all privileges on *.* to 'root'@'%' identified by 'fengkuang' with grant option; flush privileges; select user,host from user;"
 BLOCK
-#自动备份数据库
+#每天0时自动备份数据库
 mkdir   -p     /home/wpdbbackup/
 echo       '
-0 0 * * *  mysqldump     -uroot     -pfengkuang     wordpress   >    /home/wpdbbackup/$(date "+\%Y\%m\%d \%H:\%M:\%S ")wordpress.sql
+0 0 * * *     mysqldump     -uroot     -pfengkuang     wordpress   >    /home/wpdbbackup/$(date "+\%Y\%m\%d \%H:\%M:\%S ")wordpress.sql
 '  |  crontab
 crontab    -l
 service   cron   restart
