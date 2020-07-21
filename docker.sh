64f6b3b96326ff6a65098a9ed39d02835ca7c715
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-21 20:44:49 +0800

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,8 +3,8 @@ apt   update
 apt   full-upgrade  -y    --fix-missing
 apt   autoremove    -y
 apt   install       -y      apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl         -fsSL          https://download.docker.com/linux/ubuntu/gpg    |  apt-key add -
-add-apt-repository         "deb [arch=amd64] https://download.docker.com/linux/ubuntu    $(lsb_release -cs)    stable"
+curl         -fsSL          https://download.docker.com/linux/debian/gpg    |  apt-key add -
+add-apt-repository         "deb [arch=amd64] https://download.docker.com/linux/debian    $(lsb_release -cs)    stable"
 apt   update
 apt   install       -y      docker-ce docker-ce-cli containerd.io
 docker      run      hello-world
