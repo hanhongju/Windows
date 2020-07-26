8a98396edd98d7b94e8cc5e3970328d4c44eed97
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-26 14:15:06 +0800

    Update cloudflare_dns.sh

--- cloudflare_dns.sh
+++ cloudflare_dns.sh
@@ -10,13 +10,20 @@ domain=rmrf.hongju.xyz
 dynamic_ip=`curl ip.sb`
 ttl=120
 
-#创建记录
+
+<<EOF
+
+#创建域名对应IP记录
 curl -X POST "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records" \
 -H "X-Auth-Email: ${email}" \
 -H "X-Auth-Key: ${api_key}" \
 -H "Content-Type: application/json" \
 --data '{"type":"A", "name":"'${domain}'", "content":"'${dynamic_ip}'", "ttl":'${ttl}', "proxied":false}'
 
+EOF
+
+
+#更新记录
 #获取record_id参数
 record=`curl -X GET "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records" \
 -H "X-Auth-Email: ${email}" \
@@ -25,13 +32,12 @@ record=`curl -X GET "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_r
 record_id=`echo ${record} | grep "id" | awk      -F     '"'     '{print $6}'`
 echo   ${record_id}
 
-#更新记录
 curl -X PUT "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${record_id}" \
 -H "X-Auth-Email: ${email}" \
 -H "X-Auth-Key: ${api_key}" \
 -H "Content-Type: application/json" \
 --data '{"type":"A", "name":"'${domain}'", "content":"'${dynamic_ip}'", "ttl":'${ttl}', "proxied":false}'
-#至此DNS记录已经更新
+
 
 
 
