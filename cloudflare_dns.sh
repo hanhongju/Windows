7b608e96fc1ba4b69aa224735985cefada4592c2
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-26 13:48:22 +0800

    Update cloudflare_dns.sh

--- cloudflare_dns.sh
+++ cloudflare_dns.sh
@@ -1,7 +1,7 @@
 #cloudflare api添加dns记录@Debian 10
 #安装依赖
 apt  update
-apt  install   -y  curl
+apt  install   -y   curl
 #定义cloudflare api参数
 email=86606682@qq.com
 api_key=0e4c326d547e60c8901491fec0bbbfe6b8f84
