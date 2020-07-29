7fc3d87907e7f1080ac2605a999638951c365c69
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-07-29 10:56:40 +0800

    Update cloudflare_dns.sh

--- cloudflare_dns.sh
+++ cloudflare_dns.sh
@@ -2,10 +2,10 @@
 
 #安装脚本
 echo '
+
 #安装依赖
 apt  update
 apt  install   -y   curl openssl libssl-dev ca-certificates
-
 #定义cloudflare api参数
 email=86606682@qq.com
 api_key=0e4c326d547e60c8901491fec0bbbfe6b8f84
@@ -13,10 +13,6 @@ zone_id=5bb519b973ac6d3df07806e97bc958df
 domain=rmrf.hongju.xyz
 dynamic_ip=`curl ip.sb`
 ttl=120
-
-
-
-
 #更改域名后要创建DNS记录
 <<EOF
 
@@ -28,9 +24,6 @@ curl -X POST "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records"
 --data '{"type":"A", "name":"'${domain}'", "content":"'${dynamic_ip}'", "ttl":'${ttl}', "proxied":false}'
 
 EOF
-
-
-
 #获取record_id参数
 record=`curl -X GET "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records" \
 -H "X-Auth-Email: ${email}" \
@@ -38,10 +31,6 @@ record=`curl -X GET "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_r
 -H "Content-Type: application/json" `
 record_id=`echo ${record} | grep "id" | awk      -F     '"'     '{print $6}'`
 echo   ${record_id}
-
-
-
-
 #更新记录
 curl -X PUT "https://api.cloudflare.com/client/v4/zones/${zone_id}/dns_records/${record_id}" \
 -H "X-Auth-Email: ${email}" \
