9160094ef1c2b5321d63a88b253701fe88bc34fa
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-08-03 01:24:33 +0800

    Update Postfix.sh

--- Postfix.sh
+++ Postfix.sh
@@ -1,9 +1,24 @@
 
 
+site=mail.hongju.fun
+
 apt    update
 apt    full-upgrade    -y
 apt    autoremove      -y
 apt    purge           -y         apache2 nginx
 apt    install         -y         python3-pip net-tools postfix postfix-mysql postfix-doc mailutils
+#安装Certbot
+pip3   install     cryptography --upgrade
+pip3   install     certbot
+#申请SSL证书
+certbot    certonly    --standalone    --agree-tos     -n     -d      $site     -m    86606682@qq.com 
+rm       -rf     /home/keys/
+mkdir    -p      /home/keys/
+cp       /etc/letsencrypt/live/$site/fullchain.pem       /home/keys/fullchain.pem
+cp       /etc/letsencrypt/live/$site/privkey.pem         /home/keys/privkey.pem
+chmod    -Rf     777       /home/
+
+
+
 
 
