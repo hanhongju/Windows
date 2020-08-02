14fb9917b1556d059a6eacb24ec23d084bc35360
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-08-03 01:08:44 +0800

    Update Postfix.sh

--- Postfix.sh
+++ Postfix.sh
@@ -1,5 +1,9 @@
 
 
-apt  update
-apt  install  -y   postfix postfix-mysql postfix-doc mailutils
+apt    update
+apt    full-upgrade    -y
+apt    autoremove      -y
+apt    purge           -y         apache2 nginx
+apt    install         -y         python3-pip net-tools postfix postfix-mysql postfix-doc mailutils
+
 
