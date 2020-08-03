5fdf136569950ea540fdc041fd8e104edd608841
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-08-03 15:35:25 +0800

    Update iRedMail.sh

--- iRedMail.sh
+++ iRedMail.sh
@@ -9,6 +9,7 @@ hostname -f
 echo  "mx"    >    /etc/hostname
 echo  "127.0.0.1       mail.hanhongju.com      mx      localhost      localhost.localdomain"       >        /etc/hosts
 hostname -f
+#如果没有正确读取本机名称，reboot重启
 #下载安装脚本，执行
 wget   https://github.com/iredmail/iRedMail/releases/download/1.3.1/iRedMail-1.3.1.tar.gz     -cP      /home/
 cd    /home/
