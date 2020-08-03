6c10426b3ad70bfdeb6492d51876feac1ea9e282
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-08-04 01:24:16 +0800

    Update and rename minecraft_JAVA to minecraft.sh

---
+++ minecraft.sh
@@ -0,0 +1,78 @@
+#MineCraft JAVA版服务器搭建脚本@Debian 9
+apt    update
+apt    full-upgrade   -y
+apt    autoremove     -y
+apt    install        -y        wget unzip zip screen default-jdk nginx
+#架设下载服务器
+echo     'server {listen 80;listen [::]:80;root /home/;}'   >   /etc/nginx/sites-enabled/default
+service   nginx   restart
+#下载1.15.2版本mc服务器文件
+wget      https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar     -cP     /home/mcserver/
+#打开虚拟终端，后台运行服务器
+screen   -R      mc
+cd       /home/mcserver/
+java     -jar    server.jar    nogui
+sed      -i     's/eula=false/eula=true/g'       eula.txt
+java     -jar    server.jar    nogui
+#关闭shell，连接服务器，开玩
+
+#关闭所有screen终端和服务器
+netstat  -plunt | grep 'java'
+screen   -ls
+screen   -S   mc   -X   quit
+
+#备份服务器，将mcserver目录压缩为mcserver.zip文件
+cd      /home/
+zip     -q       mcjava.zip          -r     ./mcserver/
+
+#到新服务器，上传备份文件至/home/并解压
+rm      -rf     /home/mcserver/
+unzip   -qo     /home/mcjava.zip     -d      /home/
+#打开服务器终端，开玩
+
+
+
+
+
+
+
+
+#Minecraft基岩版服务器架设@Debian 10
+#安装常用软件包
+apt update
+apt full-upgrade   -y
+apt autoremove     -y
+apt install        -y   wget unzip zip libcurl4-openssl-dev nginx
+#架设下载服务器
+echo     'server {listen 80;listen [::]:80;root /home/;}'   >   /etc/nginx/sites-enabled/default
+service   nginx   restart
+#下载基岩版mc服务器文件
+wget     https://minecraft.azureedge.net/bin-linux/bedrock-server-1.14.60.5.zip      -cP     /home/
+unzip   -qo   /home/bedrock-server-1.14.60.5.zip    -d    /home/mcserver/
+chmod   -Rf    777    /home/
+#启动服务器
+cd /home/mcserver/
+(LD_LIBRARY_PATH=. ./bedrock_server&)
+#关闭shell，连接服务器，开玩
+
+#关闭服务器
+pkill -9 bedrock_server
+
+#备份服务器，将worlds目录压缩为worlds.zip文件
+cd      /home/mcserver/
+zip     -q       /home/worlds.zip     -r     ./worlds/
+
+#到新服务器，上传备份文件至/home/并解压
+rm      -rf     /home/mcserver/worlds/
+unzip   -qo     /home/worlds.zip     -d     /home/mcserver/
+#启动服务器，开玩
+#win10版MC本地数据库文件在：
+#C:\Users\hongju\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds
+
+
+
+
+
+
+
+
