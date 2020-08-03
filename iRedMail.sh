b606082c32f78c6d737218098821370d4f256a36
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-08-03 15:23:39 +0800

    Update iRedMail.sh

--- iRedMail.sh
+++ iRedMail.sh
@@ -4,20 +4,16 @@ apt    update
 apt    full-upgrade    -y    --fix-missing
 apt    autoremove      -y
 apt    install         -y      gzip curl net-tools
-#更改rDNS
+#更改主机名称
 hostname -f
-echo  "hj"    >    /etc/hostname
-dynamic_ip=`curl ip.sb`
-echo  $dynamic_ip
-sed      -i       "/$dynamic_ip/d"           /etc/hosts
-echo  "$dynamic_ip     mail.hanhongju.com      hj"       >>        /etc/hosts
+echo  "mx"    >    /etc/hostname
+echo  "127.0.0.1       mail.hanhongju.com      mx      localhost      localhost.localdomain"       >        /etc/hosts
 hostname -f
 #下载安装脚本，执行
-wget  https://github.com/iredmail/iRedMail/releases/download/1.3.1/iRedMail-1.3.1.tar.gz    -cP     /home/
+wget   https://github.com/iredmail/iRedMail/releases/download/1.3.1/iRedMail-1.3.1.tar.gz     -cP      /home/
 cd    /home/
-tar zxf iRedMail-1.3.1.tar.gz
+tar    zxf    iRedMail-1.3.1.tar.gz
 cd     iRedMail-1.3.1
 bash   iRedMail.sh
 
 
-netstat -plunt
