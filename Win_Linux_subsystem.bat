80c577629404dc769da04d881b9035d41cf2abe1
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-10-14 21:20:51 +0800

    Create Win_Linux_subsystem.bat

---
+++ Win_Linux_subsystem.bat
@@ -0,0 +1,3 @@
+#powershell以管理员身份启动
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
