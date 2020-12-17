242689069e8022304350f09bea2391be08d5ccf7
hanhongju <63781648+hanhongju@users.noreply.github.com>
2020-12-17 16:46:05 +0800

    Create boostcpu.bat

---
+++ boostcpu.bat
@@ -0,0 +1,6 @@
+#  https://www.idleleo.com/02/4258.html
+#  提升CPU性能
+
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v FeatureSettingsOverride /t REG_DWORD /d 3 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v FeatureSettingsOverrideMask /t REG_DWORD /d 3 /f
+
