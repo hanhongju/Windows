b21c4677a356f5847aaf95c2dfb18ed02030ad52
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-01-07 14:02:09 +0800

    Delete Win_Linux_subsystem.bat

--- Win_Linux_subsystem.bat
+++
@@ -1,3 +0,0 @@
-#powershell以管理员身份启动
-dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
