0aa93cf2f7dbf6e3ccfb4384ebf325177ddfd4c9
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-01-07 20:33:04 +0800

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -14,9 +14,9 @@ service       nginx         stop
 certbot       certonly    --standalone    --agree-tos     -n     -d     $site     -m    86606682@qq.com 
 #配置证书每月1日自动更新，每天备份数据库
 echo   '
-0 0 1 * *     service       nginx     stop
-1 0 1 * *     certbot       renew
-2 0 1 * *     service       nginx     start
+0 0 1 * *     service   nginx   stop
+1 0 1 * *     certbot   renew
+2 0 1 * *     service   nginx   start
 0 0 * * *     mkdir        -p        /home/dbbackup/
 0 0 * * *     mysqldump    -uroot    -pfengkuang     wordpress     >      /home/dbbackup/$(date +\%Y\%m\%d)wordpress.sql
 0 1 * * *     apt   full-upgrade   -y
