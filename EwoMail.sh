f542a37e3450b66b6f40b4effa79ff976b5d4f07
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-01-09 14:56:56 +0800

    Update EwoMail.sh

--- EwoMail.sh
+++ EwoMail.sh
@@ -5,7 +5,7 @@ sed       -i       ''s/SELINUX\=.*/SELINUX\=disabled/g''        /etc/sysconfig/s
 #添加SWAP缓存空间
 if        [[   $(free -m   2>&1)    =~     Swap\:.*[1-9].*   ]]
 then      echo   ''已经有SWAP，无需重复配置''
-else      dd    if=/dev/zero of=/mnt/swap bs=1M count=2000
+else      dd    if=/dev/zero of=/mnt/swap bs=1M count=3000
           mkswap   /mnt/swap
           swapon   /mnt/swap
           echo    '/mnt/swap swap swap defaults 0 0'      >>       /etc/fstab
@@ -13,10 +13,11 @@ fi
 
 #安装EwoMail
 yum    install   git    -y 
-cd     /root
+cd     /root/
 git    clone     https://gitee.com/laowu5/EwoMail.git
-cd     /root/EwoMail/install
-sh    ./start.sh    $site
+cd     /root/EwoMail/install/
+sed    -i    ''s/yum\ install\ epel-release/yum\ install\ epel-release\ \-y/g''     start.sh
+bash   start.sh    $site
 
 
 
