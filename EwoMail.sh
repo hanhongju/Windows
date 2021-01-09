e5048b1d2be98eba2a64addf8cc4b497e8cd0a71
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-01-09 14:30:44 +0800

    Create EwoMail.sh

---
+++ EwoMail.sh
@@ -0,0 +1,17 @@
+#关闭Selinux
+sed     -i     ''s/SELINUX\=.*/SELINUX\=disabled/g''     /etc/sysconfig/selinux
+#添加SWAP缓存空间
+if        [[   $(free -m   2>&1)    =~     Swap\:.*[1-9].*   ]]
+then      echo   ''已经有SWAP，无需重复配置''
+else      dd    if=/dev/zero of=/mnt/swap bs=1M count=2000
+          mkswap   /mnt/swap
+          swapon   /mnt/swap
+          echo    '/mnt/swap swap swap defaults 0 0'      >>       /etc/fstab
+fi
+
+#安装EwoMail
+yum    install   git    -y 
+cd     /root
+git    clone     https://gitee.com/laowu5/EwoMail.git
+cd     /root/EwoMail/install
+
