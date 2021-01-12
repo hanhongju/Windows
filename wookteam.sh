a175fbabcb388c2c5061faccc6616e6075ac2f10
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-01-12 22:10:19 +0800

    Update wookteam.sh

--- wookteam.sh
+++ wookteam.sh
@@ -2,7 +2,7 @@ yum install -y     gcc gcc-c++ make kernel-devel nginx
 yum install -y php72w php72w-cli php72w-devel php72w-gd php72w-fpm php72w-mbstring php72w-pear php72w-xml php72w-xmlrpc php72w-common php72w-pdo php72w-mysqli 
 yum update -y   kernel 
 
-yum localinstall http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm 
+yum localinstall  -http://nginx.org/packages/centos/7/noarch/RPMS/nginx-release-centos-7-0.el7.ngx.noarch.rpm 
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm 
 rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 
