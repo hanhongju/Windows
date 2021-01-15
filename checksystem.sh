033b79404d42a30ebfc05def3accbce946a5b381
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-01-15 23:57:43 +0800

    Update checksystem.sh

--- checksystem.sh
+++ checksystem.sh
@@ -1,5 +1,5 @@
-if     [[  $(grep  PRETTY_NAME=   /etc/os-release)   =~    'CentOS Linux 8'       ]]   ;then   SYSTEM='CentOS  8'
-elif   [[  $(grep  PRETTY_NAME=   /etc/os-release)   =~    'Debian GNU/Linux 10'  ]]   ;then   SYSTEM='Debian  10'
+if     [[  $(grep  PRETTY_NAME=   /etc/os-release)   =~    'CentOS Linux 8'       ]]   ;then   SYSTEM='CentOS8'
+elif   [[  $(grep  PRETTY_NAME=   /etc/os-release)   =~    'Debian GNU/Linux 10'  ]]   ;then   SYSTEM='Debian10'
 fi
 echo $SYSTEM
 
