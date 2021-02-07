1f0d16ae20bbc0367fe1c7d0f86878981f8e5cb0
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-02-07 12:27:00 +0800

    Create kmsserver.sh

---
+++ kmsserver.sh
@@ -0,0 +1,16 @@
+#创建KMS服务器用以激活 Windows 10 系统 @ Debian 10
+apt     update
+apt     install   -y   wget
+wget    https://github.com/Wind4/vlmcsd/releases/download/svn1113/binaries.tar.gz   -cP   /home/kms/
+tar     -zxvf     /home/kms/binaries.tar.gz   -C   /home/kms/
+/home/kms/binaries/Linux/intel/static/vlmcsd-x64-musl-static
+ss      -plnt
+
+
+
+#在Windows中用管理员身份登录Powershell输入以下代码
+slmgr /skms  ali.hongju.live
+slmgr /ato
+slmgr /xpr
+
+
