336d7325026b22645553f2d9dea5f9fdde167438
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-03-27 13:22:24 +0800

    Update zip.sh

--- zip.sh
+++ zip.sh
@@ -1,1 +1,9 @@
-unzip     -qo      /home/besttrace/besttrace4linux.zip     -d     /home/besttrace/
+
+
+
+
+unzip      -qo        /home/besttrace/besttrace4linux.zip     -d      /home/besttrace/
+tar        -xf        /home/latest-zh_CN.tar.gz               -C      /home/
+
+
+
