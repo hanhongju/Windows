51594c50649cae5e8acab141189d70ca19db9486
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-03-27 13:20:01 +0800

    Update besttrace4linux.sh

--- besttrace4linux.sh
+++ besttrace4linux.sh
@@ -1,8 +1,7 @@
 #BestTrace4Linux 查看VPS回程节点@Debian 10
 apt       update
 apt       install    -y     unzip wget
-wget      https://github.com/zhucaidan/BestTrace-Linux/raw/master/besttrace4linux.zip      -cP     /home/besttrace/
-unzip     -qo      /home/besttrace/besttrace4linux.zip     -d     /home/besttrace/
+wget      https://github.com/zhucaidan/BestTrace-Linux/raw/master/besttrace      -cP     /home/besttrace/
 chmod     -Rf      777      /home/besttrace/
 /home/besttrace/besttrace   60.218.218.145
 
