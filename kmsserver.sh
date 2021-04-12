5086ede8730a5e050e1a967bb1bea93d44adc348
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-04-12 20:37:25 +0800

    Update kmsserver.sh

--- kmsserver.sh
+++ kmsserver.sh
@@ -4,7 +4,7 @@ apt     install   -y   wget
 yum     install   -y   wget
 wget    https://github.com/Wind4/vlmcsd/releases/download/svn1113/binaries.tar.gz   -cP   /home/kms/
 tar     -xf     /home/kms/binaries.tar.gz   -C   /home/kms/
-cp      /home/kms/binaries/Linux/intel/static/vlmcsd-x64-musl-static      /usr/bin/vlmcsd
+\cp     -f      /home/kms/binaries/Linux/intel/static/vlmcsd-x86-musl-static     /usr/bin/vlmcsd
 vlmcsd
 ss      -plnt
 
