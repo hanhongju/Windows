cb5d16368e5c920692daa338a0a095060c4da871
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-05-16 00:44:27 +0800

    Update minecraft.sh

--- minecraft.sh
+++ minecraft.sh
@@ -34,9 +34,7 @@ tar    -Pxf     /home/mc.tar
 #Minecraft基岩版服务器架设@Debian 10
 #安装常用软件包
 apt update
-apt full-upgrade   -y
-apt autoremove     -y
-apt install        -y   wget unzip zip libcurl4-openssl-dev nginx
+apt install        -y   wget unzip zip libcurl4-openssl-dev
 #下载基岩版mc服务器文件
 wget     https://minecraft.azureedge.net/bin-linux/bedrock-server-1.14.60.5.zip      -cP     /home/
 unzip   -qo   /home/bedrock-server-1.14.60.5.zip    -d    /home/mcserver/
@@ -49,17 +47,12 @@ cd /home/mcserver/
 #关闭服务器
 pkill -9 bedrock_server
 
-#备份服务器，将worlds目录压缩为worlds.zip文件
-cd      /home/mcserver/
-zip     -q       /home/worlds.zip     -r     ./worlds/
-#架设下载服务器
-echo     'server {listen 80;listen [::]:80;root /home/;}'   >   /etc/nginx/sites-enabled/default
-service   nginx   restart
+#备份服务器
+tar    -Pcf     /home/mc.tar      /home/mcserver/worlds/
+#还原服务器
+tar    -Pxf     /home/mc.tar
 
-#到新服务器，上传备份文件至/home/并解压
-rm      -rf     /home/mcserver/worlds/
-unzip   -qo     /home/worlds.zip     -d     /home/mcserver/
-#启动服务器，开玩
+
 #win10版MC本地数据库文件在：
 #C:\Users\hongju\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds
 
