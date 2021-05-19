d984660182f0eef6b74fbdac28cf1e506500768d
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-05-20 00:04:48 +0800

    Update qBittorrent.sh

--- qBittorrent.sh
+++ qBittorrent.sh
@@ -2,17 +2,7 @@
 site=bt.hanhongju.com
 #安装软件申请证书
 apt    update
-apt    install   -y       nginx certbot qbittorrent-nox
-systemctl     stop        nginx apache2
-certbot       certonly    --standalone -n --agree-tos -m 86606682@qq.com -d $site
-chmod         -R   777    /etc/letsencrypt/
-#配置证书自动更新
-echo    "
-0 0 1 * *     systemctl     stop        nginx apache2
-1 0 1 * *     certbot       renew
-2 0 1 * *     chmod         -R   777    /etc/letsencrypt/
-3 0 * * *     systemctl     restart     nginx
-"       |     crontab
+apt    install   -y    qbittorrent-nox
 #添加用户
 adduser      bt    --system     --group
 #为qbittorrent-nox创建一个systemd服务文件
@@ -38,11 +28,6 @@ server {
 server_name   www.example.com;
 listen 80;
 listen [::]:80;
-listen 443 ssl;
-listen [::]:443 ssl;
-ssl_certificate          /etc/letsencrypt/live/www.example.com/fullchain.pem;
-ssl_certificate_key      /etc/letsencrypt/live/www.example.com/privkey.pem;
-if ( $scheme = http ){return 301 https://$server_name$request_uri;}
 location /      {
 proxy_pass                 http://127.0.0.1:8080/;
 proxy_http_version         1.1;
