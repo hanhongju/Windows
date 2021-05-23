b54869537945da776b5b44e705f8a9ebff541fcc
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-05-23 19:51:53 +0800

    Update qBittorrent.sh

--- qBittorrent.sh
+++ qBittorrent.sh
@@ -1,6 +1,6 @@
 # qBittorrent安装脚本 @ Debian 10
 apt    update
-apt    install   -y    nginx qbittorrent-nox
+apt    install   -y    qbittorrent-nox
 adduser     bt   --system     --group
 #为qbittorrent-nox创建一个systemd服务文件
 echo   ' 
@@ -19,26 +19,11 @@ Restart=on-failure
 WantedBy=multi-user.target
 '        >          /etc/systemd/system/qbittorrent-nox.service
 systemctl   enable    qbittorrent-nox
-#配置nginx反代
-echo  '
-server {
-server_name   bt.hanhongju.com;
-listen 80;
-listen [::]:80;
-location /      {
-proxy_pass                 http://127.0.0.1:8080/;
-proxy_http_version         1.1;
-proxy_set_header           X-Forwarded-Host        $http_host;
-http2_push_preload on;     #NGINX从1.13.9版本开始支持HTTP/2服务端推送
-}
-}
-'         >         /etc/nginx/sites-enabled/qbittorrent.conf
 #重启服务
 systemctl   restart   qbittorrent-nox nginx
 sleep       1s
 ss         -plnt
-#回显监听端口
-#用户名admin，密码adminadmin，默认下载目录/home/bt/Downloads/
+#网页端口8080，用户名admin，密码adminadmin，默认下载目录/home/bt/Downloads/
 
 
 
