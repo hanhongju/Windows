8f8751a35429fd5ebd7c115dd36c3385c897a2cc
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-09-03 01:02:24 +0800

    Update ftp.sh

--- ftp.sh
+++ ftp.sh
@@ -1,12 +1,16 @@
 # Ftp服务器安装脚本 @ Debian 10 or Ubuntu 20.04
 apt update  -y
 apt install -y vsftpd
-echo '
+ipv4=121.37.203.125
+echo "
 write_enable=YES
 chroot_local_user=YES
 allow_writeable_chroot=YES
 local_root=/home/ftp/
-'  >>  /etc/vsftpd.conf
+pasv_address=$ipv4
+"  >>  /etc/vsftpd.conf
+sed       -i       ''s/listen\=.*/listen\=YES/g''        /etc/vsftpd.conf
+sed       -i       ''s/listen\_ipv6\=.*/listen\_ipv6\=NO/g''        /etc/vsftpd.conf
 systemctl restart vsftpd
 mkdir -p  /home/ftp/
 chmod 777 /home/ftp/
