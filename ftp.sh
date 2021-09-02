4ce4feb7a55a2c625b669d80818b221bcfa869ca
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-09-03 00:19:46 +0800

    Create ftp.sh

---
+++ ftp.sh
@@ -0,0 +1,20 @@
+
+apt update  -y
+apt install -y vsftpd
+echo '
+write_enable=YES
+chroot_local_user=YES
+allow_writeable_chroot=YES
+local_root=/home/ftp/
+'  >>  /etc/vsftpd.conf
+systemctl restart vsftpd
+mkdir -p  /home/ftp/
+chmod 777 /home/ftp/
+useradd  -m  hongju   -d   /home/hongju/
+passwd   hongju
+
+
+
+
+
+
