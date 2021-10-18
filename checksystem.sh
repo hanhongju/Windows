0bcc660f34baf51bdf0c0535d2be40031a99580a
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-10-18 20:07:36 +0800

    Delete checksystem.sh

--- checksystem.sh
+++
@@ -1,8 +0,0 @@
-if     [[  $(grep  PRETTY_NAME=   /etc/os-release)   =~    'CentOS Linux 8'       ]]   ;then   SYSTEM='CentOS8'
-elif   [[  $(grep  PRETTY_NAME=   /etc/os-release)   =~    'Debian GNU/Linux 10'  ]]   ;then   SYSTEM='Debian10'
-fi
-echo $SYSTEM
-
-
-
-
