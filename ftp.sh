7c92cecc83e38f5a851590345c81be1fb2dabb17
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-10-18 20:05:10 +0800

    Update ftp.sh

--- ftp.sh
+++ ftp.sh
@@ -2,8 +2,8 @@
 # 定义服务器地址
 server=hw.thenote.site
 # 安装
-apt update  -y
-apt install -y vsftpd
+apt     -y     update    
+apt     -y     install      vsftpd
 ipv4=$(ping -c 2 $server | head -2 | tail -1 | awk '{print $5}' | sed 's/[(:)]//g')
 echo $ipv4
 echo "
@@ -13,15 +13,15 @@ allow_writeable_chroot=YES
 local_root=/home/ftp/
 utf8_filesystem=YES
 pasv_address=$ipv4
-"  >>  /etc/vsftpd.conf
-sed      -i       ''s/listen\=.*/listen\=YES/g''        /etc/vsftpd.conf
-sed      -i       ''s/listen\_ipv6\=.*/listen\_ipv6\=NO/g''        /etc/vsftpd.conf
-rm       -rf    /home/ftp/
-mkdir    -p     /home/ftp/
-chmod    777    /home/ftp/
-useradd  -m     hongju  -d  /usr/hongju/
-echo     -e     "fengkuang\nfengkuang" |  passwd  hongju
-systemctl restart vsftpd
+"           >>        /etc/vsftpd.conf
+sed         -i        ''s/listen\=.*/listen\=YES/g''        /etc/vsftpd.conf
+sed         -i        ''s/listen\_ipv6\=.*/listen\_ipv6\=NO/g''        /etc/vsftpd.conf
+rm          -rf       /home/ftp/
+mkdir       -p        /home/ftp/
+chmod       777       /home/ftp/
+useradd     -m        hongju      -d      /usr/hongju/
+echo        -e        "fengkuang\nfengkuang" |  passwd  hongju
+systemctl   restart   vsftpd
 
 
 
