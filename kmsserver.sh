b01bf0796a78a696506d1460b6719a62d9bf72d1
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-10-18 18:49:52 +0800

    Update kmsserver.sh

--- kmsserver.sh
+++ kmsserver.sh
@@ -1,10 +1,10 @@
 #创建KMS服务器用以激活 Windows 10 系统
-apt     -y    update    
-apt     -y    install    wget
-yum     -y    install    wget
-wget    -c    https://github.com/Wind4/vlmcsd/releases/download/svn1113/binaries.tar.gz   -P   /home/kms/
-tar     -xf   /home/kms/binaries.tar.gz   -C   /home/kms/
-\cp     -f    /home/kms/binaries/Linux/intel/static/vlmcsd-x86-musl-static     /usr/bin/vlmcsd
+apt     -y     update    
+apt     -y     install    wget
+yum     -y     install    wget
+wget    -c     https://github.com/Wind4/vlmcsd/releases/download/svn1113/binaries.tar.gz   -P   /home/kms/
+tar     -Pxf   /home/kms/binaries.tar.gz   -C   /home/kms/
+\cp     -f     /home/kms/binaries/Linux/intel/static/vlmcsd-x86-musl-static     /usr/bin/vlmcsd
 vlmcsd
 
 
