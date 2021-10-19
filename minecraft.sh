4f9a97d47103de40938befe8bd2499d00ad1d3d1
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-10-19 22:19:48 +0800

    Update minecraft.sh

--- minecraft.sh
+++ minecraft.sh
@@ -3,19 +3,22 @@ apt      -y      update
 apt      -y      install      wget default-jdk screen
 #下载mc服务器1.16.5
 wget     -c      https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar     -P     /home/mcserverjava/
-#打开虚拟终端，后台运行服务器
-screen   -R      mc
+#配置服务，启动服务器
+echo   "eula=true"     >    /home/mcserverjava/eula.txt
+echo   ' 
+[Unit]
+Description=Minecraft server
+[Service]
+Type=forking
+WorkingDirectory=    /home/mcserverjava/
+ExecStart=           java     -jar    /home/mcserverjava/server.jar     nogui
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target
+'               >       /etc/systemd/system/mcserver.service
+systemctl       enable       mcserver
+systemctl       restart      mcserver
 
-
-java     -jar    /home/mcserverjava/server.jar     nogui
-sed      -i      's/eula=false/eula=true/g'        /home/mcserverjava/eula.txt
-java     -jar    /home/mcserverjava/server.jar     nogui
-#关闭shell开玩
-
-#关闭所有screen终端和服务器
-ss       -plnt | grep 'java'
-screen   -ls
-screen   -S   mc   -X   quit
 
 #备份服务器
 tar    -Pcf     /home/mcjava.tar      /home/mcserverjava/
@@ -23,42 +26,10 @@ tar    -Pcf     /home/mcjava.tar      /home/mcserverjava/
 tar    -Pxf     /home/mcjava.tar
 
 
-
-
-
-
-
-
-
-# Minecraft基岩版服务器架设 @ Debian
-#安装常用软件包
-apt    update
-apt    install        -y   wget unzip zip libcurl4-openssl-dev
-#下载基岩版mc服务器文件
-wget    https://minecraft.azureedge.net/bin-linux/bedrock-server-1.14.60.5.zip      -cP     /home/
-#wget   https://img.zeruns.tech/down/bedrock-server-1.16.20.03.zip      -cP     /home/
-unzip   -qo   /home/bedrock-server-1.14.60.5.zip    -d    /home/mcserverbedrock/
-chmod   -Rf    777    /home/
-#启动服务器
-cd /home/mcserverbedrock/
-(LD_LIBRARY_PATH=. ./bedrock_server&)
-#关闭shell开玩
-
-#关闭服务器
-pkill   -9    bedrock_server
-
-#备份服务器
-tar    -Pcf     /home/mcbedrockworlds.tar      /home/mcserverbedrock/worlds/
-#还原服务器
-tar    -Pxf     /home/mcbedrockworlds.tar
-
 # 官网下载地址
 # https://www.minecraft.net/zh-hans/download
 # Win10版MC本地数据库文件在
 # C:\Users\hongju\AppData\Local\Packages\Microsoft.MinecraftUWP_8wekyb3d8bbwe\LocalState\games\com.mojang\minecraftWorlds
-
-
-
 
 
 
