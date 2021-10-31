4c47c87f3190b69a10a73641c5595e5b09e91487
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-11-01 00:38:45 +0800

    Update vsftpd.sh

--- vsftpd.sh
+++ vsftpd.sh
@@ -6,11 +6,10 @@ apt         -y        update
 apt         -y        install      vsftpd
 ipv4=$(ping -c 2 $server | head -2 | tail -1 | awk '{print $5}' | sed 's/[(:)]//g')
 echo        $ipv4
-sed         -i        '/pasv_address/d'                              /etc/vsftpd.conf
 echo                  "pasv_address=$ipv4"             >>            /etc/vsftpd.conf
-sed         -i        's/.*\(write_enable=\).*/\1YES/g'              /etc/vsftpd.conf
-sed         -i        's/.*\(listen=\).*/\1YES/g'                    /etc/vsftpd.conf
-sed         -i        's/.*\(listen_ipv6=\).*/\1NO/g'                /etc/vsftpd.conf
+sed         -i        's/^#\(write_enable=\)/\1/g'                   /etc/vsftpd.conf
+sed         -i        's/\(listen=\).*/\1YES/g'                      /etc/vsftpd.conf
+sed         -i        's/\(listen_ipv6=\).*/\1NO/g'                  /etc/vsftpd.conf
 useradd     -m        hongju      -d      /usr/hongju/
 echo        -e        "fengkuang\nfengkuang"           |             passwd  hongju
 systemctl   enable    vsftpd
