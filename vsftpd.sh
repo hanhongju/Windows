50a21ac465da3e65b30a3c49d26b20359b969514
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-11-01 00:25:59 +0800

    Update vsftpd.sh

--- vsftpd.sh
+++ vsftpd.sh
@@ -5,7 +5,7 @@ server=tx.thenote.site
 apt     -y     update    
 apt     -y     install      vsftpd
 ipv4=$(ping -c 2 $server | head -2 | tail -1 | awk '{print $5}' | sed 's/[(:)]//g')
-echo $ipv4
+echo        $ipv4
 echo                  "pasv_address=$ipv4"             >>            /etc/vsftpd.conf
 sed         -i        's/.*\(write_enable=\).*/\1YES/g'              /etc/vsftpd.conf
 sed         -i        's/.*\(listen=\).*/\1YES/g'                    /etc/vsftpd.conf
