8083bfec567e7b39cf36cb5be2159268a834f388
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-11-02 23:32:13 +0800

    Update trojanclient.sh

--- trojanclient.sh
+++ trojanclient.sh
@@ -1,7 +1,7 @@
 # trojan客户端安装脚本 @ Debian 10 or Ubuntu 20
 site=cloud2.thenote.site
 apt           -y   update    
-apt           -y   install  trojan
+apt           -y   install  trojan tsocks
 echo '
 {"run_type": "client"
 ,"local_addr": "127.0.0.1"
@@ -18,7 +18,21 @@ sed         -i        ''s/www.example.com/$site/g''       /etc/trojan/config.jso
 systemctl   enable    trojan
 systemctl   restart   trojan
 trojan      -t
+
+
+
+#设置tsocks透明代理
+echo '
+server       =  127.0.0.1
+server_type  =  5
+server_port  =  1080
+default_user =  none
+default_pass =  none
+'          >              /etc/tsocks.conf
+#测试代理可用性
+tsocks    wget   -c   https://cn.wordpress.org/latest-zh_CN.tar.gz   -P   /home/wordpress/
 ss          -plnt   |   awk 'NR>1 {print $4,$6}'   |   column   -t
+
 
 
 
