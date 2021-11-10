c3d7a18ed6151e242f799407cbaf5d5c8eeb45ae
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-11-10 19:42:36 +0800

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -30,8 +30,8 @@ include        fastcgi_params;
 sed           -i           ''s/post\_max\_size\ \=.*/post\_max\_size\ \=200M/g''                    /etc/php/7.4/fpm/php.ini
 sed           -i           ''s/upload\_max\_filesize\ \=.*/upload\_max\_filesize\ \=200M/g''        /etc/php/7.4/fpm/php.ini
 sed           -i           ''s/max\_execution\_time\ \=.*/max\_execution\_time\ \=300/g''           /etc/php/7.4/fpm/php.ini
+echo          "client_header_buffer_size 512k;   large_client_header_buffers 4 512k;"       >       /etc/nginx/conf.d/414.conf
 systemctl     restart      php7.4-fpm
-#重启服务
 systemctl     enable       nginx cron
 systemctl     restart      nginx cron
 php           -v
