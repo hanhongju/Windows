8653bb0310558eaf9880dc00a135d408a5dbbfce
hanhongju <63781648+hanhongju@users.noreply.github.com>
2021-12-25 10:49:17 +0800

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -8,8 +8,9 @@ echo    '
 0 1 * * *     mkdir         -p          /home/wordpressbackup/
 0 2 * * *     mysqldump     -uroot      -pfengkuang     wordpress     >      /home/wordpress/wordpress.sql
 0 3 * * *     tar           -cf         /home/wordpressbackup/$(date +\%Y\%m\%d)wordpress.tar        -P       /home/wordpress/
-0 4 * * *     apt           -y          full-upgrade
-0 5 * * *     apt           -y          autoremove
+0 4 * * *     apt           -y          update
+0 5 * * *     apt           -y          full-upgrade
+0 6 * * *     apt           -y          autoremove
 '       |     crontab
 systemctl     restart       cron
 #创建nginx配置文件
