48fc3f31bced118c9dfd7582e43553ce9f6496a6
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-01-19 23:47:29 +0800

    Update realm.sh

--- realm.sh
+++ realm.sh
@@ -1,14 +1,22 @@
 #realm端口转发代理f2pool矿池 @ Debian 10
 apt      -y      update    
 apt      -y      install      wget
-wget    -c   https://github.com/zhboner/realm/releases/download/v1.4/realm    -P    /usr/bin/
+wget     -c      https://github.com/zhboner/realm/releases/download/v1.4/realm    -P    /usr/bin/
+echo '
+{
+    "listening_addresses": ["0.0.0.0"],
+    "listening_ports": ["8000-9000"],
+    "remote_addresses": ["eth.f2pool.com"],
+    "remote_ports": ["6688"]
+}
+'        >           /ect/realm.conf
 #配置服务，启动服务器
 echo   ' 
 [Unit]
 Description=relay to f2pool
 [Service]
 Type=simple
-ExecStart=/usr/bin/realm -l 0.0.0.0:8000-9000 -r eth.f2pool.com:6688 
+ExecStart=/usr/bin/realm   -c    /ect/realm.conf
 Restart=on-failure
 [Install]
 WantedBy=multi-user.target
