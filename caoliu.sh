d28cc626a751502e0b68f47b5c8541e9d7d48cf7
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-01-21 18:21:07 +0800

    Update caoliu.sh

--- caoliu.sh
+++ caoliu.sh
@@ -3,5 +3,9 @@
 apt   update  -y
 apt   install  -y  python3  
 pip3   install requests 
-wget        https://raw.githubusercontent.com/chuxiuhong/spider1024/master/Caoliu.py    -O    /home/Caoliu.py
-python3    /home/Caoliu.py
+wget        https://raw.githubusercontent.com/chuxiuhong/spider1024/master/Caoliu.py    -O    /root/caoliu.py
+python3    /root/caoliu.py
+
+
+
+
