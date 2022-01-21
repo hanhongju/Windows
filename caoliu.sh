109df531b200b94f5a11c9b6c7551c160ebc824c
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-01-21 18:17:21 +0800

    Create caoliu.sh

---
+++ caoliu.sh
@@ -0,0 +1,7 @@
+
+
+apt   update  -y
+apt   install  -y  python3  
+pip3   install requests 
+wget        https://raw.githubusercontent.com/chuxiuhong/spider1024/master/Caoliu.py    -O    /home/Caoliu.py
+python3    /home/Caoliu.py
