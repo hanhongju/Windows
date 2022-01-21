22da38a42a749614f8c9ee8519a6da70a939e0f7
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-01-21 18:56:53 +0800

    Update caoliu.sh

--- caoliu.sh
+++ caoliu.sh
@@ -1,11 +1,34 @@
 
 
 apt        update  -y
-apt        install  -y  python3  
-pip3       install requests 
-wget        https://raw.githubusercontent.com/chuxiuhong/spider1024/master/Caoliu.py    -O    /root/caoliu.py
-python3    /root/caoliu.py
+apt        install  -y  python3  git
+pip3       install requests bs4  scrapy crawl
 
 
+git clone https://github.com/Nymphet/t66y-spider.git
 
+cd t66ySpider
 
+# 爬達蓋爾的旗幟的帖子，保存帖子标题和图片地址
+
+scrapy crawl DaGaiEr
+
+# 爬新時代的我們的帖子，保存帖子标题和图片地址
+
+scrapy crawl XinShiDai
+
+# 收集亚洲无码原创区帖子内的图片地址和种子链接，也保存帖子标题和帖子url
+
+scrapy crawl YaZhouWuMa
+
+# 亚洲无码转帖交流区，保存图片地址，种子链接，帖子标题，帖子url
+
+scrapy crawl YaZhouWuMaZhuanTie
+
+# 亚洲有码原创区，同上
+
+scrapy crawl YaZhouYouMa
+
+# 亚洲有码转帖交流区，同上
+
+scrapy crawl YaZhouYouMaZhuanTie
