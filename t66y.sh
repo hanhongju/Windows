45a2d29aaabd755ba834fcfd685a3f30de79dcfe
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-01-22 13:25:48 +0800

    Update t66y.sh

--- t66y.sh
+++ t66y.sh
@@ -1,13 +1,25 @@
 # 利用爬虫下载草榴【新時代的我們】和【達蓋爾的旗幟】两个板块帖子内的图片
 # https://github.com/yikesoftware/t66y_spider
 apt          -y     update
-apt          -y     install      python3 wget
+apt          -y     install      python3 wget nginx
 pip3         install             bs4 requests tqdm lxml
 wget         -c     https://raw.githubusercontent.com/hanhongju/my_script/master/t66y_spider.py
 python3      t66y_spider.py -h
 python3      t66y_spider.py -c 2
+
+
+
 #打包下载的文件
-tar           -cf         /home/wordpress/t66y.tar        -P       ./t66y/
+echo '
+server {
+server_name cloud1.thenote.site;
+listen 80;
+listen [::]:80;
+root      /home/;
+}
+'             >            /etc/nginx/sites-enabled/download.conf
+systemctl     restart      nginx
+tar           -cf          /home/t66y.tar        -P       ./t66y/
 
 
 
