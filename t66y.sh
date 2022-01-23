9019bd473e89b10f940f1f15e2a716865c644270
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-01-23 17:42:55 +0800

    Update t66y.sh

--- t66y.sh
+++ t66y.sh
@@ -1,7 +1,7 @@
 # 利用爬虫下载草榴【新時代的我們】和【達蓋爾的旗幟】两个板块帖子内的图片
 # https://github.com/yikesoftware/t66y_spider
 apt          -y     update
-apt          -y     install      python3 wget nginx
+apt          -y     install      python3 python3-pip wget nginx
 pip3         install             bs4 requests tqdm lxml
 wget         -c     https://raw.githubusercontent.com/hanhongju/my_script/master/t66y_spider.py
 python3      t66y_spider.py -h
