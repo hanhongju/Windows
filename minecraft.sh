bf05b1e4ff6c3d3885cbe352253b3a0c244c12ca
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-05-07 02:37:31 +0800

    Update minecraft.sh

--- minecraft.sh
+++ minecraft.sh
@@ -1,8 +1,8 @@
 # MineCraft JAVA版服务器搭建脚本 @ Debian
 apt      -y      update    
 apt      -y      install      wget default-jdk
-#下载mc服务器1.16.5
-wget     -c      https://launcher.mojang.com/v1/objects/1b557e7b033b583cd9f66746b7a9ab1ec1673ced/server.jar     -P     /home/mcserverjava/
+#下载mc服务器1.18.2
+wget     -c      https://launcher.mojang.com/v1/objects/c8f83c5655308435b3dcf03c06d9fe8740a77469/server.jar     -P     /home/mcserverjava/
 #配置服务，启动服务器
 echo   'eula=true'      >       /home/mcserverjava/eula.txt
 echo   ' 
