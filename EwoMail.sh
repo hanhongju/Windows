eaaea951c43f446767cb0526a164ef6725ace310
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-05-09 17:10:11 +0800

    Update EwoMail.sh

--- EwoMail.sh
+++ EwoMail.sh
@@ -30,13 +30,6 @@ systemctl   restart     postfix dovecot nginx
 echo        "服务器管理账户为admin，密码为ewomail123。"
 
 
-directsetup () {
-apt  -y install wget
-wget -c https://raw.githubusercontent.com/hanhongju/my_script/master/EwoMail.sh
-bash    EwoMail.sh
-}
-
-
 
 
 
