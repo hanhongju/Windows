038085eeaab35a851d397b793d5355922be01e61
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-05-21 17:41:30 +0800

    Create qBittorrent.sh

---
+++ qBittorrent.sh
@@ -0,0 +1,24 @@
+# qBittorrent安装脚本 @ Debian 10
+apt     -y    update
+apt     -y    install     qbittorrent-nox
+#配置服务，启动服务器
+echo   ' 
+[Unit]
+Description=qBittorrent Command Line Client
+After=network.target
+[Service]
+Type=forking
+ExecStart=/usr/bin/qbittorrent-nox -d --webui-port=8088
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target
+'           >          /etc/systemd/system/qbittorrent-nox.service
+systemctl   daemon-reload
+systemctl   enable     qbittorrent-nox
+systemctl   restart    qbittorrent-nox
+#网页端口8088，用户名admin，密码adminadmin，默认下载目录/Downloads/
+
+
+
+
+
