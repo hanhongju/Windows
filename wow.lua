54a9ab6d87802b9ad0cf35cf25b1d4a369c77a28
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-05-31 01:36:22 +0800

    Update wow.lua

--- wow.lua
+++ wow.lua
@@ -1,16 +1,16 @@
 HandoutMaterials ()  {
-  /script SelectGossipAvailableQuest(1)
-  /script CompleteQuest()
-  /script GetQuestReward()
-  }
+/script SelectGossipAvailableQuest(1)
+/script CompleteQuest()
+/script GetQuestReward()
+}
 
 
 DefineMessage () {
-  /script msg="what i want to say"
-  }
+/script msg="what i want to say"
+}
 
 
 BoardcastMessage () {
-  /script SendChatMessage(msg,"YELL");SendChatMessage(msg,"CHANNEL",nil,1);
-  }
+/script SendChatMessage(msg,"YELL");SendChatMessage(msg,"CHANNEL",nil,1);
+}
 
