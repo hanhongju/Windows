4d0003381df4a258468094541f21b0df4efe9697
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-06-15 14:40:29 +0800

    Update EwoMail.sh

--- EwoMail.sh
+++ EwoMail.sh
@@ -33,13 +33,4 @@ echo        "服务器管理账户为admin，密码为ewomail123。"
 
 
 
-directsetup () {
-yum  -y install wget
-wget -c https://raw.githubusercontent.com/hanhongju/my_script/master/EwoMail.sh
-bash    EwoMail.sh
-}
-
-
-
-
 
