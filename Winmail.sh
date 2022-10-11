647b362b30498d97f1839cd54fa1621126f265ed
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-10-11 13:09:17 +0800

    Update Winmail.sh

--- Winmail.sh
+++ Winmail.sh
@@ -1,13 +1,28 @@
-CentOs 7.5-7.9
+# WinMail 安装脚本 @ CentOS 7.5-7.9
 
-
-yum           -y     install wget tar telnet net-tools mlocate bind-utils
-wget          https://cdn.winmail.cn/download/winmail-pro-5.0-0728-x86_64.tar.gz        -P          /home/
-rm            -rf     /home/wordpress/
-tar           -xf     /home/winmail-pro-5.0-0728-x86_64.tar.gz                          -C          /home/
-systemctl     stop winmail
+#添加SWAP缓存空间
+if        [[   $(free  -m  |  awk   'NR==3{print $2}'   2>&1)    >   3000   ]]
+then      echo   ''已经有SWAP，无需重复配置''
+else      echo   ''添加SWAP空间，大小4000M''
+          dd       if=/dev/zero of=/mnt/swap bs=1M count=4000
+          mkswap   /mnt/swap
+          swapon   /mnt/swap
+          echo    '/mnt/swap swap swap defaults 0 0'      >>       /etc/fstab
+fi
+#安装WinMail
+yum           -y       install     wget tar telnet net-tools mlocate bind-utils
+wget          https://cdn.winmail.cn/download/winmail-pro-5.0-0728-x86_64.tar.gz         -P          /home/
+tar           -xf      /home/winmail-pro-5.0-0728-x86_64.tar.gz                          -C          /home/
+systemctl     stop     winmail
 bash          /home/winmail/install.sh
 
+
+
+
+#重启服务
+systemctl     restart  winmail
+#访问   http://mail.hanhongju.com:6080/admin/
+#进行邮箱域名配置
 
 
 
