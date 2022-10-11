09200a36af4e1f9afcad2a38e6b64a7166defe6a
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-10-11 12:54:53 +0800

    Update Winmail.sh

--- Winmail.sh
+++ Winmail.sh
@@ -1,1 +1,14 @@
+CentOs 7.5-7.9
+
+
+yum           -y     install wget tar telnet net-tools mlocate bind-utils
+wget          https://cdn.winmail.cn/download/winmail-pro-5.0-0728-x86_64.tar.gz        -P          /home/
+rm            -rf     /home/wordpress/
+tar           -xf     /home/winmail-pro-5.0-0728-x86_64.tar.gz                          -C          /home/
+systemctl     stop winmail
+bash          /home/winmail/install.sh
+
+
+
+
 
