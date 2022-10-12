061a5ed13b0cd817856a5419bc6a564a16434d36
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-10-12 20:59:23 +0800

    Update iRedMail.sh

--- iRedMail.sh
+++ iRedMail.sh
@@ -8,15 +8,23 @@ else      echo   ''添加SWAP空间，大小4000M''
           swapon   /mnt/swap
           echo    '/mnt/swap swap swap defaults 0 0'      >>       /etc/fstab
 fi
-#配置hostname
-echo      "127.0.0.1   mail.hanhongju.com   mail   localhost"    >    /etc/hosts
-reboot
 #安装iRedMail
 apt       -y    update
 apt       -y    install tar gzip wget
 wget      -c    https://github.com/iredmail/iRedMail/archive/1.6.2.tar.gz -P /home/
 tar       -xf   /home//1.6.2.tar.gz -C /home/
 bash      /home/iRedMail-1.6.2/iRedMail.sh
+
+
+
+
+#配置hostname
+hostname () {
+echo      "mail"      >      /etc/hostname
+echo      "127.0.0.1   mail.hanhongju.com   mail   localhost"    >    /etc/hosts
+reboot
+
+}
 
 
 
