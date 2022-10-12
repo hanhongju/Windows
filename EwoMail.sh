b79c06670c7540ded3b7c7910d929d6eb64953c5
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-10-12 22:52:18 +0800

    Update EwoMail.sh

--- EwoMail.sh
+++ EwoMail.sh
@@ -1,11 +1,11 @@
 # EwoMail 安装脚本 @ CentOS 7
 site=hanhongju.com
 #关闭Selinux
-sed       -i       ''s/SELINUX=.*/SELINUX=disabled/g''        /etc/sysconfig/selinux
+sed       -i       "s/SELINUX=.*/SELINUX=disabled/g"        /etc/sysconfig/selinux
 #添加SWAP缓存空间
 if        [[   $(free  -m  |  awk   'NR==3{print $2}'   2>&1)    >   3000   ]]
-then      echo   ''已经有SWAP，无需重复配置''
-else      echo   ''添加SWAP空间，大小4000M''
+then      echo   "已经有SWAP，无需重复配置"
+else      echo   "添加SWAP空间，大小4000M"
           dd       if=/dev/zero of=/mnt/swap bs=1M count=4000
           mkswap   /mnt/swap
           swapon   /mnt/swap
@@ -17,14 +17,14 @@ cd          /root/
 git         clone      https://github.com/gyxuehu/EwoMail.git
 tar         -Pcf       /root/EwoMail.tar     /root/EwoMail
 #tar        -Pxf       /root/EwoMail.tar
-sed         -i         ''s/yum install epel-release.*/yum install epel-release -y/g''     /root/EwoMail/install/start.sh
+sed         -i         "s/yum install epel-release.*/yum install epel-release -y/g"     /root/EwoMail/install/start.sh
 #设定脚本工作目录，不要更改否则会出现安装失败
 cd          /root/EwoMail/install/
 bash        start.sh    $site
 #安装后的常规配置
-echo        ''127.0.0.1 mail.$site smtp.$site imap.$site''                               >>          /etc/hosts
-sed         -i          ''s/listen.*/listen\ 80\;/g''           /ewomail/nginx/conf/vhost/rainloop.conf
-sed         -i          ''s/listen.*/listen\ 8010\;/g''         /ewomail/nginx/conf/vhost/ewomail-admin.conf
+echo        "127.0.0.1 mail.$site smtp.$site imap.$site"                         >>          /etc/hosts
+sed         -i          "s/listen.*/listen\ 80\;/g"           /ewomail/nginx/conf/vhost/rainloop.conf
+sed         -i          "s/listen.*/listen\ 8010\;/g"         /ewomail/nginx/conf/vhost/ewomail-admin.conf
 #重启服务
 systemctl   daemon-reload
 systemctl   restart     postfix dovecot nginx
