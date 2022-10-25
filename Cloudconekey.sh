aafee28745a2e49508d6140476eaca5eb0b4aed5
hanhongju <63781648+hanhongju@users.noreply.github.com>
2022-10-25 20:44:48 +0800

    Update Cloudconekey.sh

--- Cloudconekey.sh
+++ Cloudconekey.sh
@@ -1,7 +1,7 @@
 # Cloudcone安装公钥
 apt  update
 apt  -y install curl
-curl -o cc-ikey -L web.cloudc.one/sh/key && sh cc-ikey CwnTua2U4SrW
+curl -o cc-ikey -L web.cloudc.one/sh/key && sh cc-ikey 24C1w2B3gP1V
 
 
 
