6a786e7dd4f125ced4f5a105b875855d5530975f
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-01-09 11:51:11 +0800

    Create resilio-sync.sh

---
+++ resilio-sync.sh
@@ -0,0 +1,16 @@
+
+
+
+
+sudo    su
+apt     -y     update    
+apt     -y     install    wget
+wget    https://download-cdn.resilio.com/2.7.3.1381/Debian/resilio-sync_2.7.3.1381-1_amd64.deb    -O    resilio-sync.deb
+dpkg    -i     resilio-sync.deb
+systemctl enable resilio-sync
+systemctl start resilio-sync
+
+
+
+
+
