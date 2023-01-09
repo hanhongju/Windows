441ee5ca2b1a46c04997dbf3b048324df22df19b
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-01-09 10:40:50 +0800

    Update vsftpd.sh

--- vsftpd.sh
+++ vsftpd.sh
@@ -4,7 +4,7 @@ server=cloud1.thenote.live
 # 安装
 sudo        su
 apt         -y        update    
-apt         -y        install      vsftpd
+apt         -y        install      vsftpd net-tools
 ipv4=$(ping -c 2 $server | head -2 | tail -1 | awk '{print $5}' | sed 's/[(:)]//g')
 echo        $ipv4
 echo                  "pasv_address=$ipv4"             >>            /etc/vsftpd.conf
@@ -15,7 +15,7 @@ useradd     -m        hhj      -d      /usr/hhj/
 echo        -e        "hhj\nhhj"           |             passwd  hhj
 systemctl   enable    vsftpd
 systemctl   restart   vsftpd
-
+netstat     -plnt
 
 
 
