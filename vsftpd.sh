e4fc736b399ddfa76390fcbba206aeec03aae3a7
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-01-09 10:47:58 +0800

    Update vsftpd.sh

--- vsftpd.sh
+++ vsftpd.sh
@@ -1,11 +1,11 @@
 # Ftp服务器安装脚本 @ Debian 10 or Ubuntu 20.04
 # 定义服务器地址
-server=cloud1.thenote.live
+site=cloud1.thenote.live
 # 安装
 sudo        su
 apt         -y        update    
 apt         -y        install      vsftpd net-tools
-ipv4=$(ping -c 2 $server | head -2 | tail -1 | awk '{print $5}' | sed 's/[(:)]//g')
+ipv4=$(ping -c 2 $site | head -2 | tail -1 | awk '{print $5}' | sed 's/[(:)]//g')
 echo        $ipv4
 echo                  "pasv_address=$ipv4"             >>            /etc/vsftpd.conf
 sed         -i        's/^#\(write_enable=\)/\1/g'                   /etc/vsftpd.conf
