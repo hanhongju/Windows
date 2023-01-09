4db4e33b5ebfb3c024e68f4472526ab59e60f42e
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-01-09 11:52:43 +0800

    Update resilio-sync.sh

--- resilio-sync.sh
+++ resilio-sync.sh
@@ -2,14 +2,14 @@
 
 
 
-sudo    su
-apt     -y     update    
-apt     -y     install    wget
-wget    https://download-cdn.resilio.com/2.7.3.1381/Debian/resilio-sync_2.7.3.1381-1_amd64.deb    -O    resilio-sync.deb
-dpkg    -i     resilio-sync.deb
-systemctl enable resilio-sync
-systemctl start resilio-sync
-
+sudo       su
+apt        -y      update    
+apt        -y      install    wget
+wget       https://download-cdn.resilio.com/2.7.3.1381/Debian/resilio-sync_2.7.3.1381-1_amd64.deb    -O    resilio-sync.deb
+dpkg       -i      resilio-sync.deb
+systemctl  enable  resilio-sync
+systemctl  start   resilio-sync
+netstat    -plnt
 
 
 
