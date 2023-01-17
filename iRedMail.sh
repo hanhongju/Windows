4a676f16dd369d4be6fea606db442c4ca693808d
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-01-17 22:45:55 +0800

    Update iRedMail.sh

--- iRedMail.sh
+++ iRedMail.sh
@@ -16,7 +16,7 @@ sysctl    kernel.hostname=mail.hanhongju.com
 apt       -y    update
 apt       -y    install tar gzip wget
 wget      https://github.com/iredmail/iRedMail/archive/1.6.2.tar.gz    -cP    /home/
-tar       -xf   /home/1.6.2.tar.gz    -C   /home/
+tar       -xf   /home/1.6.2.tar.gz    -C    /home/
 bash      /home/iRedMail-1.6.2/iRedMail.sh
 #访问      http://mail.hanhongju.com/mail/
 
