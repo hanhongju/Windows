9885260920f220f4a041baa73789aaed8c5ef772
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-01-17 21:19:19 +0800

    Update resilio-sync.sh

--- resilio-sync.sh
+++ resilio-sync.sh
@@ -1,6 +1,6 @@
-#私有云盘resilio-sync服务器搭建 @ Debian 10
+#私有云盘resilio-sync服务器搭建 @ Debian
 sudo       su
-apt        -y      update    
+apt        -y      update
 apt        -y      install    wget
 wget       -c      https://download-cdn.resilio.com/2.7.3.1381/Debian/resilio-sync_2.7.3.1381-1_amd64.deb      -O      resilio-sync.deb
 dpkg       -i      resilio-sync.deb
