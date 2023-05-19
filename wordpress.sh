060f249432aa37c8612fa35aecb16be765cad97c
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-05-19 15:52:29 +0800

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -30,7 +30,7 @@ listen 443 ssl;
 listen [::]:443 ssl;
 ssl_certificate           /home/fullchain.pem;
 ssl_certificate_key       /home/privkey.pem;
-if  ( $scheme = http )   {return 301 https://$server_name$request_uri;}
+if  ( $scheme = http )   {return 301 https://$host$request_uri;}
 root      /home/wordpress/;
 index     index.php index.html index.htm;
 location ~ \.php$ {
