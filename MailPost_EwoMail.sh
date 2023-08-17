5fe0e823d00e284b0f9dbb055fe62846211c5ccd
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-08-17 22:27:51 +0800

    Update MailPost_EwoMail.sh

--- MailPost_EwoMail.sh
+++ MailPost_EwoMail.sh
@@ -1,8 +1,5 @@
-# EwoMail 安装脚本 @ CentOS 7
 site=hanhongju.com
-#关闭Selinux
 sed       -i       "s/SELINUX=.*/SELINUX=disabled/g"        /etc/sysconfig/selinux
-#添加SWAP缓存空间
 if        [[   $(free  -m  |  awk   'NR==3{print $2}'   2>&1)    >   3000   ]]
 then      echo     "已经有SWAP，无需重复配置"
 else      echo     "添加SWAP空间，大小4000M"
@@ -11,15 +8,12 @@ else      echo     "添加SWAP空间，大小4000M"
           swapon   /mnt/swap
           echo     '/mnt/swap swap swap defaults 0 0'      >>       /etc/fstab
 fi
-#安装EwoMail
 yum         -y         install        git net-tools
 cd          /root/
 git         clone      https://github.com/gyxuehu/EwoMail.git
 sed         -i         "s/yum install epel-release.*/yum install epel-release -y/g"         /root/EwoMail/install/start.sh
-#设定脚本工作目录，不要更改否则会出现安装失败
 cd          /root/EwoMail/install/
 bash        start.sh    $site
-#安装后的常规配置
 echo        "127.0.0.1 mail.$site smtp.$site imap.$site"       >>       /etc/hosts
 sed         -i          "s/listen.*/listen 80;/g"              /ewomail/nginx/conf/vhost/rainloop.conf
 sed         -i          "s/listen.*/listen 8010;/g"            /ewomail/nginx/conf/vhost/ewomail-admin.conf
@@ -29,7 +23,6 @@ sed         -i          "/bypass_spam/d"                       /etc/amavisd/amav
 echo        "
 @bypass_virus_checks_maps = (1);
 @bypass_spam_checks_maps  = (1);   "                           >>       /etc/amavisd/amavisd.conf
-#重启服务
 systemctl   daemon-reload
 systemctl   stop        clamd@amavisd
 systemctl   disable     clamd@amavisd
@@ -51,3 +44,4 @@ bash    setup.sh
 
 
 
+# EwoMail 安装脚本 @ CentOS 7
