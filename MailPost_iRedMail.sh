01c739de8f1554dfe568eca4a8f129b3077d9ae7
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-08-17 22:29:37 +0800

    Update MailPost_iRedMail.sh

--- MailPost_iRedMail.sh
+++ MailPost_iRedMail.sh
@@ -1,5 +1,3 @@
-# iRedMail @ Debian 11
-#添加SWAP缓存空间
 if        [[   $(free  -m  |  awk   'NR==3{print $2}'   2>&1)    >   3000   ]]
 then      echo     "已经有SWAP，无需重复配置"
 else      echo     "添加SWAP空间，大小4000M"
@@ -8,16 +6,19 @@ else      echo     "添加SWAP空间，大小4000M"
           swapon   /mnt/swap
           echo     '/mnt/swap swap swap defaults 0 0'      >>       /etc/fstab
 fi
-#安装前须先配置hostname
 echo      "mail"       >      /etc/hostname
 echo      "127.0.0.1   mail.hanhongju.com   mail   localhost"    >    /etc/hosts
 sysctl    kernel.hostname=mail.hanhongju.com
-#安装iRedMail
 apt       -y     update
 apt       -y     install tar gzip wget
 wget      -c     https://github.com/iredmail/iRedMail/archive/1.6.2.tar.gz
 tar       -xf    1.6.2.tar.gz     -C.
 bash      ./iRedMail-1.6.2/iRedMail.sh
+
+
+
+
+# iRedMail @ Debian 11
 #访问      http://mail.hanhongju.com/mail/
 
 
