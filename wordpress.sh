ebd4ee51d6ab068b4939349b939724bce3e3b305
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-08-20 19:47:33 +0800

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -52,7 +52,7 @@ mysql_secure_installation
 
 setupLNMP () {
 apt     -y    install    wget
-wget    https://github.com/hanhongju/my_script/raw/master/wordpress.sh    -O    setup.sh
+wget    https://raw.githubusercontent.com/hanhongju/my_script/master/wordpress.sh    -O    setup.sh
 bash    setup.sh
 
 }
