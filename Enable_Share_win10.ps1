539d6984e2d04c64e369e00d8c1578825178c180
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-12-11 22:40:27 +0800

    Update Enable_Share_win10.ps1

--- Enable_Share_win10.ps1
+++ Enable_Share_win10.ps1
@@ -11,8 +11,7 @@ secedit       /export            /cfg           security.inf
 secedit       /configure         /cfg           security_new.inf         /db        security.sdb
 #禁用“空白密码只允许控制台登录”
 reg add HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Lsa /v LimitBlankPasswordUse /t reg_dword /d 0 /f
-#强制刷新组策略，立即生效
-gpupdate /force
+
 
 
 
