2750ad5258de26596c57e579a9f5dbeeb2402c70
hanhongju <63781648+hanhongju@users.noreply.github.com>
2023-12-17 13:12:07 +0800

    Update Enable_Share_win10.ps1

--- Enable_Share_win10.ps1
+++ Enable_Share_win10.ps1
@@ -5,11 +5,11 @@
 Set-NetConnectionProfile   -Name    (Get-NetConnectionProfile).Name     -NetworkCategory    Private
 #计算机配置-》windows设置-安全设置-》本地策略-》安全选项	    帐户：来宾帐户状态	                            设置为：已启用
 net    user    guest             /active:yes
-#计算机配置-》windows设置-安全设置-》本地策略-》用户权限分配	拒绝从网络访问这台计算机	                      删除“Guest”账号
+#计算机配置-》windows设置-安全设置-》本地策略-》用户权限分配	  拒绝从网络访问这台计算机	                      删除“Guest”账号
 secedit       /export            /cfg           security.inf
 (Get-Content security.inf)       -replace      "SeDenyNetworkLogonRight.*" , "SeDenyNetworkLogonRight =" | Set-Content "security.inf"
 secedit       /configure         /cfg           security_new.inf         /db        security.sdb
-#计算机配置-》windows设置-安全设置-》本地策略-》安全选项	    帐户：使用空密码的本地帐户只允许进行控制台登录	  设置为：已禁用
+#计算机配置-》windows设置-安全设置-》本地策略-》安全选项	    帐户：使用空密码的本地帐户只允许进行控制台登录	    设置为：已禁用
 reg add HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Lsa /v LimitBlankPasswordUse /t reg_dword /d 0 /f
 
 
