a30e4a0fd433e99ceadd5519e6e6e4645ce245d2
hanhongju <63781648+hanhongju@users.noreply.github.com>
2024-04-28 00:41:13 +0800

    Create Change_OEM_type.bat

---
+++ Change_OEM_type.bat
@@ -0,0 +1,7 @@
+reg   add   HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation     /v    Model    /t   REG_SZ    /d   NB50TL   /f
+pause
+
+::修改win11下电脑型号 Change_OEM_type
+
+
+
