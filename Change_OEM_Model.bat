55482ba1783760dd25d8fc1b852c0d30bfd4203f
hanhongju <63781648+hanhongju@users.noreply.github.com>
2024-05-13 22:44:01 +0800

    Update Change_OEM_Model.bat

--- Change_OEM_Model.bat
+++ Change_OEM_Model.bat
@@ -1,9 +1,9 @@
 reg   add   HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation     /v    Model    /t   REG_SZ    /d   N960KR   /f
 pause
 
+
+
+
 ::修改win11下电脑型号 Change_OEM_type
 ::NB50TL
 ::N960KR
-
-
-
