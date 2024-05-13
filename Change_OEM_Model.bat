11d702d22f8ac821b5697d277debec696f08f23c
hanhongju <63781648+hanhongju@users.noreply.github.com>
2024-05-13 23:09:13 +0800

    Update Change_OEM_Model.bat

--- Change_OEM_Model.bat
+++ Change_OEM_Model.bat
@@ -1,4 +1,4 @@
-reg   add   HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation     /v    Model    /t   REG_SZ    /d   N960KR   /f
+reg   add   HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation       /v  Model      /t  REG_SZ      /d  N960KR      /f
 pause
 ::P775TM
 
