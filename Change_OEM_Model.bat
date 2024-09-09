532da7a15329c003061faef64083e8568883159a
hanhongju <63781648+hanhongju@users.noreply.github.com>
2024-09-09 14:40:00 +0800

    Update Change_OEM_Model.bat

--- Change_OEM_Model.bat
+++ Change_OEM_Model.bat
@@ -1,9 +1,7 @@
 reg   add   HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation      /f      /v  Model     /t  REG_SZ     /d  N960KR
-pause
-::P775TM
-::Z370PD3
+#  P775TM
+#  Z370PD3
 
 
 
-
-::修改windows11系统信息里的电脑型号
+#修改windows11系统信息里的电脑型号
