f718ed704f6cfa99ce8928db8f1039d1bbdb9da6
hanhongju <63781648+hanhongju@users.noreply.github.com>
2024-12-24 22:37:14 +0800

    Create Activation_Windows.ps1

---
+++ Activation_Windows.ps1
@@ -0,0 +1,4 @@
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd" -OutFile "C:\Users\hj\Downloads\MAS_AIO.cmd"
+Start C:\Users\hj\Downloads\MAS_AIO.cmd
+
+
