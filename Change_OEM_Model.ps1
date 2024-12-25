03f4fba8db07457b207bcfead781f6f18207aea0
hanhongju <63781648+hanhongju@users.noreply.github.com>
2024-12-25 09:54:59 +0800

    Update Change_OEM_Model.ps1

--- Change_OEM_Model.ps1
+++ Change_OEM_Model.ps1
@@ -1,8 +1,9 @@
-reg   add   HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation      /f      /v  Model     /t  REG_SZ     /d  NK50S5
+New-Item          -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation  -Force
+New-ItemProperty  -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation  -Force  -PropertyType String  -Name Model  -Value NK50S5
+Remove-Item       -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation  -Recurse
 #  N960KR
 #  P775TM
 #  Z370PD3
-
 
 
 # 修改系统设置-关于-设备规格下面的一行字，这是系统OEM信息
