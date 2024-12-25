dc879afa725c10111bc384b6ea6f55a56f01a4f6
hanhongju <63781648+hanhongju@users.noreply.github.com>
2024-12-25 14:06:30 +0800

    Update Activation.ps1

--- Activation.ps1
+++ Activation.ps1
@@ -1,7 +1,7 @@
 Invoke-WebRequest -Proxy "http://127.0.0.1:8081" `
                   -Uri "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd" `
                   -OutFile "C:\Users\Public\Downloads\MAS_AIO.cmd"
-                  
+
 Start-Process     "C:\Users\Public\Downloads\MAS_AIO.cmd"
 
 
