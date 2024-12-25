6bf9fa6f5546ece595012551147fda129dccee52
hanhongju <63781648+hanhongju@users.noreply.github.com>
2024-12-25 10:09:34 +0800

    Update OEM_Information.ps1

--- OEM_Information.ps1
+++ OEM_Information.ps1
@@ -1,10 +1,10 @@
 New-Item          -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation  -Force
 New-ItemProperty  -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation  -Force `
-                  -PropertyType String  -Name Model  -Value NK50S5
+                  -PropertyType String  -Name Model  -Value "A Property of Hanhongju"
 Remove-Item       -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation  -Recurse
-#  N960KR
-#  P775TM
-#  Z370PD3
+
+
+
 
 
 # 修改系统设置-关于-设备规格下面的一行字，这是系统OEM信息
