48be7bd9f039bd02db98fbfd0708c6290e59ff88
hanhongju <63781648+hanhongju@users.noreply.github.com>
2024-12-27 09:36:03 +0800

    Update Activation.ps1

--- Activation.ps1
+++ Activation.ps1
@@ -1,3 +1,4 @@
+#需使用http:8081代理，否则连不上github
 Invoke-WebRequest -Proxy "http://127.0.0.1:8081" `
                   -Uri "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd" `
                   -OutFile "C:\Users\Public\Downloads\MAS_AIO.cmd"
@@ -6,6 +7,14 @@ Start-Process     "C:\Users\Public\Downloads\MAS_AIO.cmd"
 
 
 
+#无需使用代理
+Invoke-WebRequest -Uri "https://ghgo.xyz/https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd" `
+                  -OutFile "C:\Users\Public\Downloads\MAS_AIO.cmd"
+
+Start-Process     "C:\Users\Public\Downloads\MAS_AIO.cmd"
 
 
-# Windows和Office激活脚本，需使用http:8081代理，否则连不上github
+
+
+
+# Windows和Office激活脚本
