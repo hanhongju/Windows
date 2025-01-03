621cf360fde6e56fcfed1eebddad4decf1cca85f
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-03 10:57:59 +0800

    Update Enable_SMB_in_Windows.ps1

--- Enable_SMB_in_Windows.ps1
+++ Enable_SMB_in_Windows.ps1
@@ -14,7 +14,7 @@ Set-SmbClientConfiguration       -EnableBandwidthThrottling  0       -EnableLarg
 # 本地组策略编辑器-计算机配置-Windows设置-安全设置-本地策略-用户权限分配：将Guest添加到“从网络访问此计算机”和“拒绝本地登录”列表，并从“拒绝从网络访问这台计算机”列表中删除
 [byte[]]$Value = @("66","00","00","00")
 Set-ItemProperty  -Path REGISTRY::HKEY_LOCAL_MACHINE\SECURITY\Policy\Accounts\S-1-5-*-501\ActSysAc `
-                  -Name "(Default)"     -Value $Value
+                  -Name "(Default)"                             -Value $Value
 # 本地组策略编辑器-计算机配置-Windows设置-安全设置-本地策略-安全选项：禁用“账户：使用空密码的本地帐户只允许进行控制台登录”
 Set-ItemProperty  -Path REGISTRY::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Lsa `
                   -Name LimitBlankPasswordUse     -Type DWord   -Value 0
