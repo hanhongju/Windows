e02fd01b5353282a9f0ab4c1b48675852ca15162
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-03 10:51:13 +0800

    Update OEM_Information.ps1

--- OEM_Information.ps1
+++ OEM_Information.ps1
@@ -1,6 +1,9 @@
-New-Item          -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation"  -Force
-Set-ItemProperty  -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation"  -Name Model  -Type String  -Value "A Property of Hanhongju"
-Remove-Item       -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation"  -Recurse  -ErrorAction SilentlyContinue
+New-Item          -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation `
+                  -Force
+Set-ItemProperty  -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation `
+                  -Name Model  -Type String  -Value "A Property of Hanhongju"
+Remove-Item       -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation `
+                  -Recurse  -ErrorAction SilentlyContinue
 
 
 
