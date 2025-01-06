abf3c56daae52e89a9a049f4b19a5aaafdf962a8
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-06 09:58:08 +0800

    Update AllowDeviceInstall.ps1

--- AllowDeviceInstall.ps1
+++ AllowDeviceInstall.ps1
@@ -1,3 +1,4 @@
+# 设备管理器没有权限安装设备时使用此代码
 Set-ItemProperty  -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\DeviceInstall\Restrictions `
                   -Name AllowAdminInstall      -Type DWord   -Value 1
 
