438eed504c4a6a61b9e7a9a0d5e5b722e7207907
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-06 10:01:34 +0800

    Update Allow_Device_Install.ps1

--- Allow_Device_Install.ps1
+++ Allow_Device_Install.ps1
@@ -1,4 +1,4 @@
-# 设备管理器没有权限安装设备时使用此代码
+# 本地组策略编辑器-计算机配置-管理模板-系统-设备安装-设备安装限制-启用“允许管理员忽略设备安装限制策略”
 Set-ItemProperty  -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\DeviceInstall\Restrictions `
                   -Name AllowAdminInstall      -Type DWord   -Value 1
 
