be66142eec4ed2fd848426a49543e90cbae3c3c3
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-08 15:53:11 +0800

    Update Allow_Device_Install.ps1

--- Allow_Device_Install.ps1
+++ Allow_Device_Install.ps1
@@ -1,11 +1,11 @@
 # Win10系统提示“系统策略禁止安装此设备，请与系统管理员联系”
-# 本地组策略编辑器-计算机配置-管理模板-系统-设备安装-设备安装限制-启用“允许管理员忽略设备安装限制策略”
+# 安装PolicyFileEditor模块
 Install-PackageProvider   -Name NuGet            -Confirm
 Set-PSRepository          -Name PSGallery        -InstallationPolicy Trusted
-# Uninstall-Module        -Name PolicyFileEditor
 Install-Module            -Name PolicyFileEditor
 Set-ExecutionPolicy       Unrestricted    -Force
 Import-Module             -Name PolicyFileEditor
+# 本地组策略编辑器-计算机配置-管理模板-系统-设备安装-设备安装限制-启用“允许管理员忽略设备安装限制策略”
 Set-PolicyFileEntry       -Path C:/Windows/system32/GroupPolicy/Machine/Registry.pol `
                           -Key  Software\Policies\Microsoft\Windows\DeviceInstall\Restrictions `
                           -ValueName AllowAdminInstall        -Type DWord      -Data 1
