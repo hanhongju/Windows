ecfdcfc59399de931d485f020d7c394f9db04819
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-08 12:02:06 +0800

    Update Allow_Device_Install.ps1

--- Allow_Device_Install.ps1
+++ Allow_Device_Install.ps1
@@ -2,7 +2,7 @@
 # 本地组策略编辑器-计算机配置-管理模板-系统-设备安装-设备安装限制-启用“允许管理员忽略设备安装限制策略”
 Install-PackageProvider   -Name NuGet            -Confirm
 Set-PSRepository          -Name "PSGallery"      -InstallationPolicy Trusted
-Uninstall-Module          -Name PolicyFileEditor
+#Uninstall-Module         -Name PolicyFileEditor
 Install-Module            -Name PolicyFileEditor
 Set-ExecutionPolicy       Unrestricted    -Force
 Import-Module             -Name PolicyFileEditor
