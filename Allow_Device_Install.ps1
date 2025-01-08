f1c3af21b2ed991c37f943a10dd2f3d5a5d91457
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-08 12:01:17 +0800

    Update Allow_Device_Install.ps1

--- Allow_Device_Install.ps1
+++ Allow_Device_Install.ps1
@@ -1,9 +1,20 @@
-# 本地组策略编辑器-计算机配置-管理模板-系统-设备安装-设备安装限制-启用“允许管理员忽略设备安装限制策略”
-Set-ItemProperty  -Path "REGISTRY::HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy Objects\*Machine\Software\Policies\Microsoft\Windows\DeviceInstall\Restrictions"  `
-                  -Name AllowAdminInstall      -Type DWord       -Value 1
-
-
-
-
-
 # Win10系统提示“系统策略禁止安装此设备，请与系统管理员联系”
+# 本地组策略编辑器-计算机配置-管理模板-系统-设备安装-设备安装限制-启用“允许管理员忽略设备安装限制策略”
+Install-PackageProvider   -Name NuGet            -Confirm
+Set-PSRepository          -Name "PSGallery"      -InstallationPolicy Trusted
+Uninstall-Module          -Name PolicyFileEditor
+Install-Module            -Name PolicyFileEditor
+Set-ExecutionPolicy       Unrestricted    -Force
+Import-Module             -Name PolicyFileEditor
+Set-PolicyFileEntry       -Path C:/Windows/system32/GroupPolicy/Machine/Registry.pol `
+-Key Software\Policies\Microsoft\Windows\DeviceInstall\Restrictions `
+-ValueName AllowAdminInstall        -Type DWord      -Data 1 
+
+
+
+
+
+# 参考文献
+# https://www.itsvse.com/thread-10340-1-1.html
+# https://stackoverflow.com/questions/69295465/windows-10-local-group-policy-edit-via-powershell
+# https://gerane.github.io/powershell/Local-gpo-powershell/
