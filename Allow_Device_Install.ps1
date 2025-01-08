b895d99a33bb12206e8e893e6203c73b02fc6dbc
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-08 12:03:33 +0800

    Update Allow_Device_Install.ps1

--- Allow_Device_Install.ps1
+++ Allow_Device_Install.ps1
@@ -1,7 +1,7 @@
 # Win10系统提示“系统策略禁止安装此设备，请与系统管理员联系”
 # 本地组策略编辑器-计算机配置-管理模板-系统-设备安装-设备安装限制-启用“允许管理员忽略设备安装限制策略”
 Install-PackageProvider   -Name NuGet            -Confirm
-Set-PSRepository          -Name "PSGallery"      -InstallationPolicy Trusted
+Set-PSRepository          -Name PSGallery        -InstallationPolicy Trusted
 #Uninstall-Module         -Name PolicyFileEditor
 Install-Module            -Name PolicyFileEditor
 Set-ExecutionPolicy       Unrestricted    -Force
