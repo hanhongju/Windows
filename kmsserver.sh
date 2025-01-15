2437ff70fb71221c09bf9411306337f06fba3b58
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-15 11:45:43 +0800

    Update kmsserver.sh

--- kmsserver.sh
+++ kmsserver.sh
@@ -37,6 +37,7 @@ activate_LTSC_office () {
 cd             "C:\Program Files\Microsoft Office\Office16"
 cd             "C:\Program Files (x86)\Microsoft Office\Office16"
 cscript.exe    ospp.vbs    /dstatus
+cscript.exe    ospp.vbs    /inpkey:XJ2XN-FW8RK-P4HMP-DKDBV-GCVGB
 cscript.exe    ospp.vbs    /sethst:kms.03k.org
 cscript.exe    ospp.vbs    /act
 #cscript.exe   ospp.vbs    /unpkey:XXXXX
@@ -48,9 +49,7 @@ cscript.exe    ospp.vbs    /act
 
 
 #参考文献
-#https://www.wenzika.com/357.html
-#http://www.kaixinit.com/info/maintenance/3031.html
-
-
-
-
+# https://www.wenzika.com/357.html
+# http://www.kaixinit.com/info/maintenance/3031.html
+# WINDOWS密钥https://learn.microsoft.com/zh-cn/windows-server/get-started/kms-client-activation-keys?tabs=server2025%2Cwindows1110ltsc%2Cversion1803%2Cwindows81
+# OFFICE密钥https://learn.microsoft.com/zh-cn/office/volume-license-activation/gvlks
