72b633a2eb887cf68622ec8a934ecee28d44f1bc
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-16 11:00:28 +0800

    Create MP3_TO_WAV.ps1

---
+++ MP3_TO_WAV.ps1
@@ -0,0 +1,12 @@
+Set-Location   -Path   $PSScriptRoot
+$Files = Get-ChildItem  -Filter  "*.mp3"  -File
+foreach ($File in $Files) {
+    $inputFile   =   $File.FullName
+    $outputFile  =   $File.BaseName  +  ".wav"
+    ffmpeg  -i $inputFile  $outputFile
+}
+
+
+
+
+# 使用ffmpeg将MP3转换为WAV，需先将ffmpeg设置在系统环境变量PATH中
