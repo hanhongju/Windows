1ab568e87e02c4b6c11d917d0d07538218358077
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-16 09:37:56 +0800

    Create SetEnvironmentVariable.ps1

---
+++ SetEnvironmentVariable.ps1
@@ -0,0 +1,18 @@
+
+
+
+# 设置环境变量
+$envName = ffmpeg
+$envValue = C:\Users\hj\Downloads\ffmpeg-master-latest-win64-gpl-shared\bin
+$envTarget = "Machine" # 环境变量目标，可以是"Machine", "User", 或 "Process"
+ 
+# 添加或更新环境变量
+[Environment]::SetEnvironmentVariable($envName, $envValue, $envTarget)
+ 
+
+
+Get-ChildItem Env:
+
+
+
+
