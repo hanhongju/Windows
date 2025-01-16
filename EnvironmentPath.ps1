57745a4fa159472bae08a085adc94e2894f0019f
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-16 12:09:38 +0800

    Update EnvironmentPath.ps1

--- EnvironmentPath.ps1
+++ EnvironmentPath.ps1
@@ -1,23 +1,26 @@
 # 定义要添加的新路径
 $NewPath = "C:\Program Files\ffmpeg-7.1-full_build\bin"
 # 获取当前用户环境变量PATH的值
-$CurrentPath = [Environment]::GetEnvironmentVariable("PATH", "Machine")
+$CurrentPath = [Environment]::GetEnvironmentVariable("PATH", "USER")
 # 检查新路径是否已存在于Path中
 if  ($CurrentPath -like "*$newPath*")   {
     Write-Host "新路径已存在于系统Path变量中，无需添加。"
 }   else   {
     # 如果不存在，将新路径添加到Path
     $UpdatedPath = "$CurrentPath;$NewPath"
-    [Environment]::SetEnvironmentVariable("PATH", $UpdatedPath, "Machine")
-    # 重新加载环境变量，使更改生效
-    $Env:Path = [Environment]::GetEnvironmentVariable("Path","Machine")
+    [Environment]::SetEnvironmentVariable("PATH", $UpdatedPath, "USER")
     Write-Host "新路径已添加到系统Path变量。"
 }
-# 验证PATH环境变量是否已成功更新
+# 重启Powershell后查看环境变量是否已正确更改
 $Env:Path  -split ";" | Format-List
 
 
 
 
-
-# 添加环境变量PATH
+# 参考文献https://blog.csdn.net/weixin_42250302/article/details/117901436
+# USER的环境变量PATH存储于REGISTRY::HKEY_CURRENT_USER\Environment\PATH中。
+# MACHINE的环境变量PATH存储于REGISTRY::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\Session Manager\Environment\PATH中。
+# 参考文献https://superuser.com/questions/867728/user-vs-system-environment-variables-do-system-variables-override-user-variabl
+# The User path is appended to the system path.
+# 参考文献https://learn.microsoft.com/en-us/answers/questions/1159843/machine-and-user-path-variables-are-not-combined-i
+# Make sure that all paths in the env file are working and not linking to a removed folder. Otherwise, Machine and User Path variables will not combine in $Env:Path on restart.
