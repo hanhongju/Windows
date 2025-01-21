799ac5b629cf634061b6c804959203fce67e97e1
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-21 13:51:15 +0800

    Update Activation.ps1

--- Activation.ps1
+++ Activation.ps1
@@ -30,7 +30,6 @@ cscript.exe    ospp.vbs    /act
 
 
 
-
 # 参考文献
 # WINDOWS密钥https://learn.microsoft.com/zh-cn/windows-server/get-started/kms-client-activation-keys
 # OFFICE密钥https://learn.microsoft.com/zh-cn/office/volume-license-activation/gvlks
