3b65a78928a421cd442237f519b6412c48e9d448
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-25 16:05:20 +0800

    Update Actv.ps1

--- Actv.ps1
+++ Actv.ps1
@@ -1,6 +1,7 @@
 # 下载MAS激活windows和office
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/refs/heads/master/MAS/All-In-One-Version-KL/MAS_AIO.cmd" `
                   -OutFile "C:\Users\Public\Downloads\MAS_AIO.cmd"
+                  
 Start-Process     "C:\Users\Public\Downloads\MAS_AIO.cmd"
 
 
