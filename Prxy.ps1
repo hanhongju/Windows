7712a03294e222c3a26102b0bdeb6b84e45ed528
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-01-25 16:00:28 +0800

    Update Prxy.ps1

--- Prxy.ps1
+++ Prxy.ps1
@@ -2,7 +2,6 @@
 $Proxy = New-Object System.Net.WebProxy
 $Proxy.Address = "http://127.0.0.1:8081"
 [System.Net.WebRequest]::DefaultWebProxy = $Proxy
-
 # 使外部命令使用代理
 $env:https_proxy = "http://127.0.0.1:8081"
 
