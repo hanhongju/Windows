162afbaa224ef9b26d277ac8a5440c9065ce41e1
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-03-04 00:36:38 +0800

    Update PT_drama_visualization.r

--- PT_drama_visualization.r
+++ PT_drama_visualization.r
@@ -1,5 +1,5 @@
 library(plotly)
-#绘制中间变量A图
+# 绘制中间变量A图
 Ti = seq(0.1, 15, by=0.1)
 Ni = seq(1, 15, by=1)
 T0 = 4
@@ -9,7 +9,7 @@ plot_ly(x = ~Ni, y = ~Ti, z = ~dA, type = "surface", color = "black") %>%
              layout(title = 
                     list(text = "dA = (1-10^(-Ti/T0))*(1+2^0.5*10^(-(Ni-1)/(N0-1)))<br />T0 = 4<br />N0 = 7"
                          , x = 0.9, y = 0.9))
-#绘制魔力值B折线图
+# 绘制魔力值B折线图
 A  = seq(0, 2000, by=50)
 B0 = 100
 L  = 300
@@ -22,4 +22,4 @@ plot_ly(x = ~A, y = ~B, type = "scatter", mode = "markers+lines") %>%
 
 
 
-#PT站魔力值计算代码
+# PT站魔力值计算代码
