8b8a7c5d125c4db7956823c015ee7e77eb9547a1
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-06-12 16:22:21 +0800

    Update OEM_Infm.ps1

--- OEM_Infm.ps1
+++ OEM_Infm.ps1
@@ -1,11 +1,17 @@
-New-Item          -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation `
-                  -Force
-Set-ItemProperty  -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation `
-                  -Name Model  -Type String  -Value "A Property of Hanhongju"
-Remove-Item       -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation `
-                  -Recurse  -ErrorAction SilentlyContinue
+# 优先显示OEMInformation
+New-Item                -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation `
+                        -Force
+Set-ItemProperty        -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation `
+                        -Name Model  -Type String  -Value "A Property of Hanhongju"
+Remove-Item             -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation `
+                        -Recurse  -ErrorAction SilentlyContinue
+# OEMInformation没有则显示下面的SystemInformation
+Set-ItemProperty        -Path REGISTRY::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\SystemInformation\ `
+                        -Name SystemProductName -Type String  -Value "N960K"
+Remove-ItemProperty     -Path REGISTRY::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\SystemInformation\ `
+                        -Name SystemProductName -ErrorAction SilentlyContinue
 
 
 
 
-# 修改设置-系统-关于-设备规格下面的一行字，这是系统OEM电脑型号
+# 修改设置-系统-系统信息-设备名字下的一行字
