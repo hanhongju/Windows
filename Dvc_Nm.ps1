33c8046d68cd632ce93ec77a593150081714c2f5
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-06-12 16:33:38 +0800

    Update Dvc_Nm.ps1

--- Dvc_Nm.ps1
+++ Dvc_Nm.ps1
@@ -2,14 +2,14 @@
 New-Item                -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\ `
                         -Force
 Set-ItemProperty        -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\ `
-                        -Name Model  -Type String  -Value "A Property of Hanhongju"
+                        -Name Model               -Type String  -Value "A Property of Hanhongju"
 Remove-Item             -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\OEMInformation\ `
-                        -Recurse  -ErrorAction SilentlyContinue
+                        -Recurse                  -ErrorAction SilentlyContinue
 # OEMInformation没有则显示下面的SystemInformation
 Set-ItemProperty        -Path REGISTRY::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\SystemInformation\ `
-                        -Name SystemProductName -Type String  -Value "N960K"
+                        -Name SystemProductName   -Type String  -Value "N960K"
 Remove-ItemProperty     -Path REGISTRY::HKEY_LOCAL_MACHINE\SYSTEM\ControlSet001\Control\SystemInformation\ `
-                        -Name SystemProductName -ErrorAction SilentlyContinue
+                        -Name SystemProductName   -ErrorAction SilentlyContinue
 
 
 
