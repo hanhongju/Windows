821b9462cb012c7270a5f89d16b631f9b465d4bd
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-06-13 11:49:03 +0800

    Update BBDown.ps1

--- BBDown.ps1
+++ BBDown.ps1
@@ -1,15 +1,8 @@
 # 在 Windows 沙盒上安装 WinGet
-$progressPreference = 'silentlyContinue'
-Write-Host "Installing WinGet PowerShell module from PSGallery..."
-Install-PackageProvider -Name NuGet -Force | Out-Null
-Install-Module -Name Microsoft.WinGet.Client -Force -Repository PSGallery | Out-Null
-Write-Host "Using Repair-WinGetPackageManager cmdlet to bootstrap WinGet..."
-Repair-WinGetPackageManager -AllUsers
-Write-Host "Done."
+Install-PackageProvider         -Name NuGet
+Install-Module                  -Name Microsoft.WinGet.Client
+Repair-WinGetPackageManager     -AllUsers
 # 安装 SDK
 winget install --accept-source-agreements Microsoft.DotNet.SDK.9
-winget install --accept-source-agreements Microsoft.DotNet.DesktopRuntime.9
-winget install --accept-source-agreements Microsoft.DotNet.AspNetCore.9
 # BBDown已经以 Dotnet Tool 形式发布
-dotnet nuget enable source nuget.org
 dotnet tool install --global BBDown
