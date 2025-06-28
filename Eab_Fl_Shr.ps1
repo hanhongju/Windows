47502a70e5c4ac6c517bb750f8ee4ab1894178c9
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-06-28 14:44:31 +0800

    Update Eab_Fl_Shr.ps1

--- Eab_Fl_Shr.ps1
+++ Eab_Fl_Shr.ps1
@@ -12,12 +12,9 @@ Set-SmbServerConfiguration       -EnableSMB2Protocol  $true          -Force
 # 客户端主机开启SMB大型MTU支持提升文件传输效率，并禁用带宽限制。
 Set-SmbClientConfiguration       -EnableBandwidthThrottling  0       -EnableLargeMtu  1      -Force
 # 本地组策略编辑器-计算机配置-Windows设置-安全设置-本地策略-用户权限分配：将Guest从“拒绝从网络访问这台计算机”列表中删除
-# 导出组策略
 secedit.exe    /export     /cfg     C:\Users\hj\Documents\gp.inf     /quiet
-# 修改组策略
-$content   =   Get-Content     C:\Users\hj\Documents\gp.inf
-$content   -replace     "SeDenyNetworkLogonRight = Guest", "SeDenyNetworkLogonRight ="    |    Set-Content     C:\Users\hj\Documents\gp.inf
-# 导入组策略，强制刷新组策略，立即生效(否则，重启后生效)：
+$content       =    Get-Content     C:\Users\hj\Documents\gp.inf
+$content       -replace     "SeDenyNetworkLogonRight = Guest", "SeDenyNetworkLogonRight ="    |    Set-Content     C:\Users\hj\Documents\gp.inf
 secedit.exe    /configure   /db   gp.sdb   /cfg    C:\Users\hj\Documents\gp.inf    /quiet
 gpupdate
 # 本地组策略编辑器-计算机配置-Windows设置-安全设置-本地策略-安全选项：禁用“账户：使用空密码的本地帐户只允许进行控制台登录”
