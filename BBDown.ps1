b572831470f37097111934fef574ce13e7ee73b3
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-07-14 15:05:14 +0800

    Delete BBDown.ps1

--- BBDown.ps1
+++
@@ -1,8 +0,0 @@
-# 在 Windows 沙盒上安装 WinGet
-Install-PackageProvider         -Name NuGet
-Install-Module                  -Name Microsoft.WinGet.Client
-Repair-WinGetPackageManager     -AllUsers
-# 安装 SDK
-winget install --accept-source-agreements Microsoft.DotNet.SDK.9
-# BBDown已经以 Dotnet Tool 形式发布
-dotnet tool install --global BBDown
