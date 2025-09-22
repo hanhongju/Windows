00ec97200d9ed9034038ca552b6cd216700e4d7b
hanhongju <63781648+hanhongju@users.noreply.github.com>
2025-09-22 09:07:47 +0800

    Update Add_Subtitle.ps1

--- Add_Subtitle.ps1
+++ Add_Subtitle.ps1
@@ -1,6 +1,8 @@
 ffmpeg   -i input.mp4   -i eng.srt   -i chs.srt   -c copy   -map 0:v   -map 0:a   -map 1   -map 2   -metadata:s:s:0 language=eng   -metadata:s:s:1 language=chs   output.mkv
 
 
+ffmpeg   -i input.mp4   -i eng.srt   -c copy   -metadata:s:s:0 language=eng   output.mkv
+
 
 
 # 参考文献 https://zhuanlan.zhihu.com/p/677539095
